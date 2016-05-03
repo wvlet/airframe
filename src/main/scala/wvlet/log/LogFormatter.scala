8f666879c8801eb0a616fcd618441cf4ead0b964
Taro L. Saito <leo@xerial.org>
2016-05-03 16:02:13 -0700

    Add loglevel definitions

---
+++ src/main/scala/wvlet/log/LogFormatter.scala
@@ -0,0 +1,14 @@
+package wvlet.log
+
+/**
+  *
+  */
+
+object LogFormatter {
+
+
+}
+
+
+
+
--- src/main/scala/wvlet/log/Logger.scala
+++ src/main/scala/wvlet/log/Logger.scala
@@ -1,33 +1,109 @@
 package wvlet.log
 
 import java.io.{ByteArrayOutputStream, PrintStream}
-import java.util.logging.Level
+import java.util.logging.{Handler, Level}
+import java.util.{Locale, logging => jl}
 
 import scala.language.experimental.macros
 
 
+/**
+  * log level definitions
+  */
+object LogLevel {
+
+  case object OFF extends LogLevel(0, Level.OFF, "off")
+  case object ERROR extends LogLevel(1, Level.SEVERE, "error")
+  case object WARN extends LogLevel(2, Level.WARNING ,"warn")
+  case object INFO extends LogLevel(3, Level.INFO, "info")
+  case object DEBUG extends LogLevel(4, Level.FINE, "debug")
+  case object TRACE extends LogLevel(5, Level.FINER, "trace")
+  case object ALL extends LogLevel(6, Level.ALL, "all")
+
+  val values = IndexedSeq(OFF, ERROR, WARN, INFO, DEBUG, TRACE, ALL)
+  private lazy val index = values.map { l => l.name.toLowerCase -> l } toMap
+
+  def apply(name: String): LogLevel = {
+    val n = name.toLowerCase(Locale.US)
+    val lv = values.find(n == _.name)
+    if (lv.isEmpty) {
+      Console.err.println(s"Unknown log level [${name}] Use info log level instead.")
+      INFO
+    }
+    else
+      lv.get
+  }
+
+  def unapply(name:String) : Option[LogLevel] = index.get(name.toLowerCase(Locale.US))
+}
+
+sealed abstract class LogLevel(val order: Int, val jlLevel: Level, val name: String) extends Ordered[LogLevel] with Serializable {
+  def compare(other: LogLevel) = this.order - other.order
+  override def toString = name
+}
+
+trait LoggerHandler {
+
+  def handler: Handler
+
+  def addTo(logger:jl.Logger) {
+    logger.addHandler(handler)
+  }
+}
+
+
+
+
 object Logger {
 
-//  lazy val configuration : Unit = {
-//    val context = LoggerFactory.getILoggerFactory().asInstanceOf[LoggerContext]
-//    val configurator = new JoranConfigurator
-//    configurator.setContext(context)
-//    context.reset()
-//
-//    if(System.console() != null) {
-//      configurator.doConfigure(getClass.getResource("/wvlet/log/logback-console.xml"))
-//    }
-//  }
+  /**
+    * Create a new {@link java.util.logging.Logger}
+    * @param name
+    * @param level
+    * @param handlers
+    * @param useParents
+    * @return
+    */
+  def getLogger(name:String,
+                level:Option[LogLevel],
+                handlers:Seq[LoggerHandler] = Seq.empty,
+                useParents: Boolean = true
+               ) : jl.Logger = {
+    val logger = jl.Logger.getLogger(name)
+    logger.clearHandlers
+    level.foreach(l => logger.setLevel(l.jlLevel))
+    handlers.foreach(h => h.addTo(logger))
+    logger.setUseParentHandlers(useParents)
+    logger
+  }
+
+  implicit class RichLogger(logger:jl.Logger) {
+    def clear {
+      clearHandlers
+      resetLogLevel
+    }
+
+    def clearHandlers {
+      for(lst <- Option(logger.getHandlers); h <- lst) {
+        logger.removeHandler(h)
+      }
+    }
+
+    def resetLogLevel {
+      logger.setLevel(null)
+    }
+  }
 }
+
 
 /**
   *
   */
-trait Logger extends Serializable {
+trait LogSupport extends Serializable {
 
   import LogMacros._
 
-  protected[this] def logger = {
+  protected[this] def logger : jl.Logger = {
     //Logger.configuration // initialize
     java.util.logging.Logger.getLogger(this.getClass.getName)
   }
