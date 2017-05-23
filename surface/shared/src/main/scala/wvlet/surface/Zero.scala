c6d7a042be6d5db02d903a1a4e4112f980fad48c
xerial <leo@xerial.org>
2017-05-22 19:44:50 -0700

    Add Zero to generate the default instance of a class

---
+++ surface/shared/src/main/scala/wvlet/surface/Zero.scala
@@ -0,0 +1,101 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package wvlet.surface
+
+import scala.reflect.ClassTag
+import scala.util.Try
+
+/**
+  * Create a default instance (zero) from Surface
+  */
+object Zero {
+
+  type ZeroValueFactory = PartialFunction[Surface, Any]
+  type SurfaceFilter = PartialFunction[Surface, Surface]
+
+  private def isPrimitive: SurfaceFilter = {
+    case p if p.isPrimitive => p
+  }
+  private def isGenericWithTypeArgs: SurfaceFilter = {
+    case g: GenericSurface if g.typeArgs.length > 0 => g
+  }
+
+  private def zeroOfPrimitives: ZeroValueFactory = isPrimitive andThen {
+    case Primitive.String => ""
+    case Primitive.Boolean => false
+    case Primitive.Short => 0.toShort
+    case Primitive.Int => 0
+    case Primitive.Long => 0L
+    case Primitive.Float => 0f
+    case Primitive.Double => 0.0
+    case Primitive.Byte => 0.toByte
+    case Primitive.Unit => null
+  }
+
+  private def zeroOfArray: ZeroValueFactory = {
+    case ArraySurface(cl, elementSurface) =>
+      ClassTag(elementSurface.rawType).newArray(0)
+  }
+
+  private def zeroOfSpecialType: ZeroValueFactory = {
+    case s if s.isOption => None
+    case s if s.rawType == classOf[Nothing] ||
+      s.rawType == classOf[AnyRef] ||
+      s.rawType == classOf[Any]
+    => null
+  }
+
+  private def zeroOfScalaCollections: ZeroValueFactory = isGenericWithTypeArgs andThen {
+    case g if classOf[Seq[_]].isAssignableFrom(g.rawType) =>
+      Seq.empty
+    case g if classOf[Map[_, _]].isAssignableFrom(g.rawType) =>
+      Map.empty
+    case g if classOf[Set[_]].isAssignableFrom(g.rawType) =>
+      Set.empty
+  }
+
+  private def zeroOfTuple: ZeroValueFactory = {
+    case t: TupleSurface =>
+      t.objectFactory.map {
+        val args = t.typeArgs.map(s => zeroOf(s))
+        _.newInstance(args)
+      }.getOrElse(null)
+  }
+
+  private def zeroOfInstantiatable: ZeroValueFactory = {
+    case t if t.objectFactory.isDefined =>
+      val args = t.params.map(x => zeroOf(x.surface))
+      Try(t.objectFactory.get.newInstance(args)).getOrElse(null)
+  }
+
+  private def fallBack: ZeroValueFactory = {
+    case _ => null
+  }
+
+  private val factory: ZeroValueFactory =
+    zeroOfPrimitives orElse
+      zeroOfArray orElse
+      zeroOfTuple orElse
+      zeroOfSpecialType orElse
+      zeroOfScalaCollections orElse
+      zeroOfInstantiatable orElse
+      fallBack
+
+  def zeroOf(surface: Surface): Any = {
+    // Dealias function resolves the actual types of aliased surfaces, tagged surfaces etc.
+    val zero = factory.apply(surface.dealias)
+    zero
+  }
+}
