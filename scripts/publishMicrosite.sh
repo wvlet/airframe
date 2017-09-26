d41037624dff0c63305b4fe2a315827af55d4a1b
Taro L. Saito <leo@xerial.org>
2017-09-26 12:12:48 -0700

    pull git before publish docs

--- scripts/publishMicrosite.sh
+++ scripts/publishMicrosite.sh
@@ -5,4 +5,8 @@ git config --global user.email "leo@xerial.org"
 git config --global user.name "Taro L. Saito"
 git config --global push.default simple
 
-./sbt docs/publishMicrosite
+git reset --hard HEAD
+git clean -f
+git checkout master
+git pull origin master
+sbt docs/publishMicrosite
