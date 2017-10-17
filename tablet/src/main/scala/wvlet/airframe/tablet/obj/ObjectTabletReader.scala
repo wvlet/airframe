d632b6fe289d426ec067f4cdc80c3c56ce3cc5ac
Taro L. Saito <leo@xerial.org>
2017-10-16 21:47:43 -0700

    Use Codec based object tablet reader/writer

--- tablet/src/main/scala/wvlet/airframe/tablet/obj/ObjectTabletReader.scala
+++ tablet/src/main/scala/wvlet/airframe/tablet/obj/ObjectTabletReader.scala
@@ -1,10 +1,10 @@
 package wvlet.airframe.tablet.obj
 
-import wvlet.airframe.tablet.msgpack.MessageCodec
-import wvlet.airframe.tablet.{Record, TabletReader}
+import org.msgpack.core.MessagePack
+import wvlet.airframe.tablet.msgpack.{MessageCodec, MessageCodecFactory}
+import wvlet.airframe.tablet.{MessagePackRecord, Record, TabletReader}
 import wvlet.log.LogSupport
 import wvlet.surface.Surface
-import wvlet.surface.reflect.SurfaceFactory
 
 import scala.reflect.runtime.{universe => ru}
 
@@ -13,16 +13,21 @@ import scala.reflect.runtime.{universe => ru}
   */
 class ObjectTabletReader[A: ru.TypeTag](input: Seq[A], codec: Map[Surface, MessageCodec[_]] = Map.empty) extends TabletReader with LogSupport {
 
-  private val cursor  = input.iterator
-  private val reader  = new ObjectReader(codec = codec)
-  private val surface = SurfaceFactory.of[A]
+  private val cursor       = input.iterator
+  private val elementCodec = new MessageCodecFactory(codec).of[A]
 
   def read: Option[Record] = {
     if (!cursor.hasNext) {
       None
     } else {
-      val record = cursor.next()
-      Some(reader.read[A](record))
+      val elem   = cursor.next()
+      val packer = MessagePack.newDefaultBufferPacker()
+      if (elem == null) {
+        packer.packArrayHeader(0)
+      } else {
+        elementCodec.pack(packer, elem)
+      }
+      Some(MessagePackRecord(packer.toByteArray))
     }
   }
 }
