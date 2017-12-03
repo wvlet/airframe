8606577d14591a6d9591ec631fcde902d743f522
Taro L. Saito <leo@xerial.org>
2017-12-03 10:01:09 -0800

    Add StringValue

--- airframe-msgpack/shared/src/main/scala/wvlet/airframe/msgpack/spi/Value.scala
+++ airframe-msgpack/shared/src/main/scala/wvlet/airframe/msgpack/spi/Value.scala
@@ -43,16 +43,73 @@ object Value {
       packer.packNil
     }
   }
-  case class BooleanValue(v:Boolean) extends Value {
-    def toJson             = if(v) "true" else "false"
-    def valueType          = ValueType.NIL
+  case class BooleanValue(v: Boolean) extends Value {
+    def toJson             = if (v) "true" else "false"
+    def valueType          = ValueType.BOOLEAN
     def toImmutable: Value = this
     def writeTo(packer: Packer): Unit = {
       packer.packBoolean(v)
     }
   }
 
-  case class
+  case class StringValue(v: String) extends Value {
+    def toJson = {
+      val b = new StringBuilder
+      appendJsonString(b, v)
+      b.result
+    }
+
+    def valueType          = ValueType.STRING
+    def toImmutable: Value = this
+    def writeTo(packer: Packer): Unit = {
+      packer.packString(v)
+    }
+  }
+
+  private[impl] def appendJsonString(sb: StringBuilder, string: String): Unit = {
+    sb.append("\"")
+    var i = 0
+    while ({ i < string.length }) {
+      val ch = string.charAt(i)
+      if (ch < 0x20) ch match {
+        case '\n' =>
+          sb.append("\\n")
+        case '\r' =>
+          sb.append("\\r")
+        case '\t' =>
+          sb.append("\\t")
+        case '\f' =>
+          sb.append("\\f")
+        case '\b' =>
+          sb.append("\\b")
+        case _ =>
+          // control chars
+          escapeChar(sb, ch)
+      } else if (ch <= 0x7f) ch match {
+        case '\\' =>
+          sb.append("\\\\")
+        case '"' =>
+          sb.append("\\\"")
+        case _ =>
+          sb.append(ch)
+      } else if (ch >= 0xd800 && ch <= 0xdfff) { // surrogates
+        escapeChar(sb, ch)
+      } else sb.append(ch)
+
+      { i += 1; i - 1 }
+    }
+    sb.append("\"")
+  }
+
+  private val HEX_TABLE = "0123456789ABCDEF".toCharArray
+
+  private def escapeChar(sb: StringBuilder, ch: Int): Unit = {
+    sb.append("\\u")
+    sb.append(HEX_TABLE((ch >> 12) & 0x0f))
+    sb.append(HEX_TABLE((ch >> 8) & 0x0f))
+    sb.append(HEX_TABLE((ch >> 4) & 0x0f))
+    sb.append(HEX_TABLE(ch & 0x0f))
+  }
 
 }
 
