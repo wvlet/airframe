db7ce9042464b92681da141e0eb484b7b44b0fe5
Taro L. Saito <leo@xerial.org>
2018-06-14 16:17:37 -0700

    Add Control class test

---
+++ airframe-control/src/test/scala/wvlet/airframe/control/ControlTest.scala
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wvlet.airframe.control
+
+import wvlet.airframe.AirframeSpec
+
+/**
+  *
+  */
+class ControlTest extends AirframeSpec {
+  "Control" should {
+    "have loan pattern" in {
+      val out = new AutoCloseable {
+        var closed: Boolean = false
+        override def close(): Unit = {
+          closed = true
+        }
+      }
+      out.closed shouldBe false
+      Control.withResource(out) { o =>
+        // do nothing
+      }
+      out.closed shouldBe true
+    }
+  }
+}
