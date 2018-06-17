2457c1041ba41426356254172b97247c2232ffc4
Taro L. Saito <leo@xerial.org>
2018-06-16 17:59:35 -0700

    Add more expressions

--- airframe-stream/src/main/scala/wvlet/airframe/stream/spi/SQL.scala
+++ airframe-stream/src/main/scala/wvlet/airframe/stream/spi/SQL.scala
@@ -13,6 +13,8 @@
  */
 package wvlet.airframe.stream.spi
 
+import java.util.Locale
+
 import wvlet.surface.Surface
 
 /**
@@ -40,25 +42,99 @@ object SQL {
   case class MapType(keyType: DataType, valueType: DataType) extends DataType
 
   trait Relation
-  case class TableScan(schema: TableSchema)                     extends Relation
-  case class TableScanFilter(schema: TableSchema, filter: Cond) extends Relation
-  case class RawSQL(sql: String, schema: Schema)                extends Relation
+  case class AliasedRelation(relation: Relation, alias: String, columnNames: Option[Seq[String]]) extends Relation
+  case class Values(rows: Seq[Expression])                                                        extends Relation
+  case class Table(schema: TableSchema)                                                           extends Relation
+  case class SubQuery(query: Query)                                                               extends Relation
+  case class RawSQL(sql: String, schema: Schema)                                                  extends Relation
+  case class Query(body: Relation, orderBy: Seq[SortItem], limit: Option[Expression])             extends Relation
+  case class Project(in: Relation, schema: Seq[Expression])                                       extends Relation
 
-  case class Project(in: Relation, schema: Seq[Expression])                                      extends Relation
-  case class Join(left: Relation, right: Relation, joinType: JoinType, cond: Cond)               extends Relation
-  case class Aggregate(in: Relation, keys: Seq[Expression], aggregate: Seq[AggregateExpression]) extends Relation
-  case class Sort(in: Relation, sortKeys: Seq[Expression])                                       extends Relation
-
+  // Joins
+  case class Join(joinType: JoinType, left: Relation, right: Relation, cond: JoinCriteria) extends Relation
   sealed trait JoinType
-  case object InnerJoin
-  case object LeftOuterJoin
-  case object RightOuterJoin
-  case object FullOuterJoin
-  case object CrossJoin
+  case object InnerJoin      extends JoinType
+  case object LeftOuterJoin  extends JoinType
+  case object RightOuterJoin extends JoinType
+  case object FullOuterJoin  extends JoinType
+  case object CrossJoin      extends JoinType
 
+  sealed trait JoinCriteria
+  case object NaturalJoin                    extends JoinCriteria
+  case class JoinUsing(columns: Seq[String]) extends JoinCriteria
+  case class JoinOn(expr: Expression)        extends JoinCriteria
+
+  case class Aggregate(in: Relation, keys: Seq[Expression], aggregate: Seq[AggregateExpression]) extends Relation
+  case class Select(item: Seq[SelectItem],
+                    isDistinct: Boolean,
+                    from: Option[Relation],
+                    where: Option[Expression],
+                    groupBy: Seq[Expression],
+                    having: Option[Expression],
+                    orderBy: Seq[SortItem],
+                    limit: Option[String])
+      extends Relation
+
+  trait SelectItem
+  case class AllColumns(prefix: Option[QName]) extends SelectItem {
+    override def toString = s"${prefix.map(x => s"${x}.*").getOrElse("*")}"
+  }
+  case class SingleColumn(expr: Expression, alias: Option[String]) extends SelectItem {
+    override def toString = alias.map(a => s"${expr} as ${a}").getOrElse(s"${expr}")
+  }
+
+  case class SortItem(sortKey: Expression,
+                      ordering: SortOrdering = Ascending,
+                      nullOrdering: NullOrdering = UndefinedOrder)
+
+  sealed trait SetOperation                                               extends Relation
+  case class Intersect(relations: Seq[Relation], isDistinct: Boolean)     extends SetOperation
+  case class Except(left: Relation, right: Relation, isDistinct: Boolean) extends SetOperation
+  case class Union(relations: Seq[Relation], isDistinct: Boolean) extends SetOperation {
+    override def toString = {
+      val name = if (isDistinct) "Union" else "UnionAll"
+      s"${name}(${relations.mkString(",")})"
+    }
+  }
+
+  sealed trait SortOrdering
+  case object Ascending  extends SortOrdering
+  case object Descending extends SortOrdering
+
+  sealed trait NullOrdering
+  case object NullIsFirst    extends NullOrdering
+  case object NullIsLast     extends NullOrdering
+  case object UndefinedOrder extends NullOrdering
+
+  // Window functions
+  case class Window(partitionBy: Seq[Expression], orderBy: Seq[SortItem], frame: Option[WindowFrame])
+
+  sealed trait FrameType
+  case object RangeFrame extends FrameType
+  case object RowsFrame  extends FrameType
+
+  sealed trait FrameBound
+  case object UnboundedPreceding extends FrameBound
+  case object Preceding          extends FrameBound
+  case object CurrentRow         extends FrameBound
+  case object Following          extends FrameBound
+  case object UnboundedFollowing extends FrameBound
+
+  case class WindowFrame(frameType: FrameType, start: FrameBound, end: Option[FrameBound])
   trait Expression
-  sealed trait Ref                   extends Expression
-  case class ColumnRef(name: String) extends Ref
+  case class QName(parts: Seq[String]) extends Expression {
+    override def toString = parts.mkString(".")
+  }
+
+  // Function
+  case class FunctionCall(name: QName, args: Seq[Expression], isDistinct: Boolean, window: Option[Window])
+      extends Expression {
+    def functionName: String = name.toString.toLowerCase(Locale.US)
+    override def toString    = s"FunctionCall(${name}, ${args.mkString(", ")}, distinct:${isDistinct}, window:${window})"
+  }
+  case class LambdaExpr(body: Expression, args: Seq[String]) extends Expression
+
+  sealed class Ref(name: QName) extends Expression
 
   sealed trait Cond                                        extends Expression
   case object NoOp                                         extends Cond
@@ -70,6 +146,31 @@ object SQL {
   case class LessThanOrEq(a: Expression, b: Expression)    extends Cond
   case class GreaterThan(a: Expression, b: Expression)     extends Cond
   case class GreaterThanOrEq(a: Expression, b: Expression) extends Cond
+  case class Between(a: Expression, b: Expression)         extends Cond
+  case class IsNull(a: Expression)                         extends Cond
+  case class IsNotNull(a: Expression)                      extends Cond
+  case class In(a: Expression, list: Seq[Expression])      extends Cond
+
+  case class IfExpr(cond: Cond, onTrue: Expression, onFalse: Expression) extends Expression
+  case class CaseExpr(operand: Option[Expression], whenClauses: Seq[Expression], defaultValue: Option[Expression])
+      extends Expression
+  case class WhenClause(operand: Expression, result: Expression) extends Expression
+
+  sealed trait ArithmeticExpression
+  // Arithmetic expr
+  abstract sealed class BinaryExprType(symbol: String)
+  case object Add      extends BinaryExprType("+")
+  case object Subtract extends BinaryExprType("-")
+  case object Multiply extends BinaryExprType("*")
+  case object Divide   extends BinaryExprType("/")
+  case object Modulus  extends BinaryExprType("%")
+
+  case class ArithmeticBinaryExpr(exprType: BinaryExprType, left: Expression, right: Expression) extends Expression
+  case class ArithmeticUnaryExpr(sign: Sign, value: Expression)                                  extends Expression
+
+  abstract sealed class Sign(symbol: String)
+  case object Positive extends Sign("+")
+  case object Negative extends Sign("-")
 
   case class AggregateExpression(func: AggregateFunction, mode: AggregateMode, isDistinct: Boolean) extends Expression
 
@@ -90,5 +191,46 @@ object SQL {
   case class Max(e: Expression)                      extends AggregateFunction
   case class First(e: Expression)                    extends AggregateFunction
   case class Last(e: Expression)                     extends AggregateFunction
+
+  // Literal
+  sealed trait Literal        extends Expression
+  case object NullLiteral     extends Literal
+  sealed trait BooleanLiteral extends Literal
+  case object TrueLiteral     extends BooleanLiteral
+  case object FalseLiteral    extends BooleanLiteral
+  case class StringLiteral(value: String) extends Literal {
+    override def toString = s"'${value}'"
+  }
+  case class TimeLiteral(value: String) extends Literal {
+    override def toString = s"time '${value}'"
+  }
+  case class TimestampLiteral(value: String) extends Literal {
+    override def toString = s"timestamp '${value}'"
+  }
+  case class DoubleLiteral(value: Double) extends Literal {
+    override def toString = value.toString
+  }
+  case class LongLiteral(value: Long) extends Literal {
+    override def toString = value.toString
+  }
+  case class IntervalLiteral(value: String, sign: Sign, startField: IntervalField, end: Option[IntervalField])
+      extends Literal
+
+  sealed trait IntervalField
+  case object Year   extends IntervalField
+  case object Month  extends IntervalField
+  case object Day    extends IntervalField
+  case object Hour   extends IntervalField
+  case object Minute extends IntervalField
+  case object Second extends IntervalField
+
+  // Value constructor
+  case class ArrayConstructor(values: Seq[Expression])                        extends Expression
+  abstract sealed class CurrentTimeBase(name: String, precision: Option[Int]) extends Expression
+  case class CurrentTime(precision: Option[Int])                              extends CurrentTimeBase("current_time", precision)
+  case class CurrentDate(precision: Option[Int])                              extends CurrentTimeBase("current_date", precision)
+  case class CurrentTimestamp(precision: Option[Int])                         extends CurrentTimeBase("current_timestamp", precision)
+  case class CurrentLocalTime(precision: Option[Int])                         extends CurrentTimeBase("localtime", precision)
+  case class CurrentLocalTimeStamp(precision: Option[Int])                    extends CurrentTimeBase("localtimestamp", precision)
 
 }
