f08a9251210f72317bb3921d0dd968d9eb9965a5
Taro L. Saito <leo@xerial.org>
2018-08-31 15:55:52 -0700

    Upgrade to scalafmt 1.5.1

--- scripts/download-scalafmt.sh
+++ scripts/download-scalafmt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-coursier bootstrap com.geirsson:scalafmt-cli_2.12:1.4.0 -r bintray:scalameta/maven -o scalafmt --standalone --main org.scalafmt.cli.Cli
+coursier bootstrap com.geirsson:scalafmt-cli_2.12:1.5.1 -r bintray:scalameta/maven -o scalafmt --standalone --main org.scalafmt.cli.Cli
 chmod +x scalafmt
