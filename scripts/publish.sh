f6f42a6b792c1a96b81f1efce69d172ba6dffd9a
Taro L. Saito <leo@xerial.org>
2018-09-24 17:46:03 -0700

    Add a script to check public IP

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" -o "$TRAVIS_BRANCH" == "$TRAVIS_TAG" ]; then
+  PUBLIC_IP=$(curl https://api.ipify.org)
+  echo "Public IP Address of this machine: ${PUBLIC_IP}"
   openssl aes-256-cbc -K $encrypted_fa45534951b5_key -iv $encrypted_fa45534951b5_iv -in travis/secrets.tar.enc -out travis/secrets.tar -d
   tar xvf travis/secrets.tar
   if [ -z "$TRAVIS_TAG" ]; then
