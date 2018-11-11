43660bb4dc5592587a85fdb27bd466c9e517a37b
Taro L. Saito <leo@xerial.org>
2018-11-11 14:52:49 -0800

    Move CodecSpec

---
+++ airframe-codec/.jvm/src/test/scala/wvlet/airframe/codec/CodecSpec.scala
@@ -0,0 +1,64 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wvlet.airframe.codec
+import wvlet.airframe.AirframeSpec
+import wvlet.airframe.msgpack.spi.MessagePack
+import wvlet.airframe.surface.Surface
+
+/**
+  *
+  */
+trait CodecSpec extends AirframeSpec {
+  def roundtrip[A](surface: Surface, v: A, expectedType: DataType = DataType.ANY): MessageHolder = {
+    roundtrip[A](MessageCodec.ofSurface(surface).asInstanceOf[MessageCodec[A]], v, expectedType)
+  }
+
+  def roundtrip[A](codec: MessageCodec[A], v: A, expectedType: DataType): MessageHolder = {
+    val h = new MessageHolder
+    debug(s"Testing roundtrip of ${v} with ${codec}")
+    val packer = MessagePack.newBufferPacker
+    codec.pack(packer, v)
+    val unpacker = MessagePack.newUnpacker(packer.toByteArray)
+    codec.unpack(unpacker, h)
+
+    h.isNull shouldBe false
+    h.hasError shouldBe false
+    h.getDataType shouldBe expectedType
+    h.getLastValue shouldBe v
+    h
+  }
+
+  def roundtripStr[A](codec: MessageCodec[A], v: A, expectedType: DataType): MessageHolder = {
+    val h = new MessageHolder
+    trace(s"Testing str based roundtrip of ${v} with ${codec}")
+    val packer = MessagePack.newBufferPacker
+    packer.packString(v.toString)
+    val unpacker = MessagePack.newUnpacker(packer.toByteArray)
+    codec.unpack(unpacker, h)
+
+    h.isNull shouldBe false
+    h.hasError shouldBe false
+    h.getDataType shouldBe expectedType
+    h.getLastValue shouldBe v
+    h
+  }
+
+  def checkCodec[A](codec: MessageCodec[A], v: A): Unit = {
+    val b = codec.toMsgPack(v)
+    val r = codec.unpackBytes(b)
+    r shouldBe defined
+    v shouldBe r.get
+  }
+
+}
