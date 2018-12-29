c7562429da643e92291f558c4fc3d8cfad3ac484
Taro L. Saito <leo@xerial.org>
2018-12-29 14:59:22 +0900

    Fix secrets

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,6 +4,7 @@ set -e
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" -o "$TRAVIS_BRANCH" == "$TRAVIS_TAG" ]; then
   PUBLIC_IP=$(curl https://api.ipify.org)
   echo "Public IP Address of this machine: ${PUBLIC_IP}"
+  openssl aes-256-cbc -K $encrypted_54ace9f5ec4e_key -iv $encrypted_54ace9f5ec4e_iv -in travis/secrets.tar.enc -out travis/secrets.tar -d
   tar xvf travis/secrets.tar
   if [ -z "$TRAVIS_TAG" ]; then
      # Publish a snapshot
