fec2b3d229559d04acc5262eda35ddd34038b3ff
Taro L. Saito <leo@xerial.org>
2018-12-29 21:07:23 +0900

    Extract schema

--- src/main/scala/wvlet/msgframe/sql/model/SQLModel.scala
+++ src/main/scala/wvlet/msgframe/sql/model/SQLModel.scala
@@ -15,30 +15,7 @@ package wvlet.msgframe.sql.model
 
 import java.util.Locale
 
-import wvlet.airframe.surface.Surface
 import wvlet.msgframe.sql.model.SQLModel.Expression
-
-object SQLSchema {
-  sealed trait Schema
-  case class AnonSchema(columns: Seq[Column])                extends Schema
-  case class TableSchema(name: String, columns: Seq[Column]) extends Schema
-
-  case class Column(name: String, columnType: DataType)
-
-  sealed trait DataType
-  case object IntegerType                                    extends DataType
-  case object FloatType                                      extends DataType
-  case object BooleanType                                    extends DataType
-  case object StringType                                     extends DataType
-  case object JSONType                                       extends DataType
-  case object BinaryType                                     extends DataType
-  case object NullType                                       extends DataType
-  case object TimestampType                                  extends DataType
-  case class ObjectType(surface: Surface)                    extends DataType
-  case class AraryType(elemType: DataType)                   extends DataType
-  case class MapType(keyType: DataType, valueType: DataType) extends DataType
-  case object AnyType                                        extends DataType
-}
 
 trait SQLModel {
   def children: Seq[SQLModel]
