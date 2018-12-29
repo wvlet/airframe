8d529ebfed7e9dbf49068bf11d803bc39063269c
Taro L. Saito <leo@xerial.org>
2018-12-29 11:11:41 +0900

    Move SQLModelPrinter

--- src/main/scala/wvlet/msgframe/sql/model/SQLModelPrinter.scala
+++ src/main/scala/wvlet/msgframe/sql/model/SQLModelPrinter.scala
@@ -1,27 +1,12 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package wvlet.msgframe.sql
+package wvlet.msgframe.sql.model
 
-import wvlet.msgframe.sql.model.SQLModel._
-import wvlet.msgframe.sql.parser.SqlBaseParser.BooleanLiteralContext
 import wvlet.log.LogSupport
-import wvlet.msgframe.sql.model.SQLModel
+import wvlet.msgframe.sql.model.SQLModel._
 
 /**
-  *
+  * Print SQLModel classes As SQL
   */
-object SQLPrinter extends LogSupport {
+object SQLModelPrinter extends LogSupport {
 
   private def unknown(e: SQLModel): String = {
     if (e != null) {
