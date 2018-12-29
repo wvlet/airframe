44bf896e8783e4a051b289e1a73c0f4a6d5af516
Taro L. Saito <leo@xerial.org>
2018-12-29 12:27:59 +0900

    Move decrypt to before_install

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,6 @@ set -e
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" -o "$TRAVIS_BRANCH" == "$TRAVIS_TAG" ]; then
   PUBLIC_IP=$(curl https://api.ipify.org)
   echo "Public IP Address of this machine: ${PUBLIC_IP}"
-  openssl aes-256-cbc -K $encrypted_28e6d45c0467_key -iv $encrypted_28e6d45c0467_iv -in travis/secrets.tar.enc -out travis/secrets.tar -d
   tar xvf travis/secrets.tar
   if [ -z "$TRAVIS_TAG" ]; then
      # Publish a snapshot
