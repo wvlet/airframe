8963d6a66369ce8558d21462473cd5fe37271349
Taro L. Saito <leo@xerial.org>
2018-12-30 21:25:57 +0900

    Use Yaml file to define test SQL queries

--- src/test/scala/wvlet/msgframe/sql/SQLBenchmark.scala
+++ src/test/scala/wvlet/msgframe/sql/SQLBenchmark.scala
@@ -2,12 +2,31 @@ package wvlet.msgframe.sql
 
 import java.io.File
 
+import wvlet.airframe.codec.MessageCodecFactory
+import wvlet.airframe.config.YamlReader
 import wvlet.log.io.IOUtil
 
 /**
   *
   */
 object SQLBenchmark {
+
+  case class TestQuery(sql: String)
+
+  def standardQueries: Seq[String] = {
+    val yaml = YamlReader.loadYamlList("msgframe-sql/src/test/resources/wvlet/msgframe/sql/standard/queries.yml")
+
+    yaml
+      .map { y =>
+        val msgpack = YamlReader.toMsgPack(y)
+        val codec   = MessageCodecFactory.defaultFactory.of[TestQuery]
+        codec.unpackMsgPack(msgpack).map { x =>
+          x.sql
+        }
+      }
+      .filter(_.isDefined)
+      .flatten
+  }
 
   def tpcDS: Seq[String] = {
     val dir = new File("msgframe-sql/src/test/resources/wvlet/msgframe/sql/tpc-ds")
