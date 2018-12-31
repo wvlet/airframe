cf7a90b60069a56aa0e72c34be0ae7a411f50bfc
Taro L. Saito <leo@xerial.org>
2018-12-31 11:01:15 +0900

    Split test query file

--- msgframe-sql/src/main/scala/wvlet/msgframe/sql/model/ModelTreePrinter.scala
+++ msgframe-sql/src/main/scala/wvlet/msgframe/sql/model/ModelTreePrinter.scala
@@ -1,10 +1,12 @@
 package wvlet.msgframe.sql.model
 import java.io.PrintWriter
 
+import wvlet.log.LogSupport
+
 /**
   *
   */
-object ModelTreePrinter {
+object ModelTreePrinter extends LogSupport {
 
   def print(m: SQLModel, out: PrintWriter, level: Int): Unit = {
     val ws = " " * (level * 2)
