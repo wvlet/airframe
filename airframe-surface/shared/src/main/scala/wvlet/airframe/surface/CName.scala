8bb207a7739d07f1e3eda62cc642432eba722b24
Naoki Takezoe <takezoe@gmail.com>
2019-01-20 01:13:05 +0900

    Refactor CName

--- airframe-surface/shared/src/main/scala/wvlet/airframe/surface/CName.scala
+++ airframe-surface/shared/src/main/scala/wvlet/airframe/surface/CName.scala
@@ -49,76 +49,77 @@ object CName {
   private val canonicalNameTable      = new WeakHashMap[String, String]
   private val naturalNameTable        = new WeakHashMap[String, String]
 
+  private def isSplitChar(c: Char)    = c.isUpper || c == '_' || c == '-' || c == ' '
+  private def isUpcasePrefix(c: Char) = c.isUpper || c.isDigit
+
   def toCanonicalName(paramName: String): String = {
     if (paramName == null) {
-      return paramName
-    };
-
-    canonicalNameTable.getOrElseUpdate(paramName, paramNameReplacePattern.matcher(paramName).replaceAll("").toLowerCase)
+      paramName
+    } else {
+      canonicalNameTable.getOrElseUpdate(paramName,
+                                         paramNameReplacePattern.matcher(paramName).replaceAll("").toLowerCase)
+    }
   }
 
   def toNaturalName(varName: String): String = {
     if (varName == null) {
-      return null
-    };
+      varName
+    } else {
+      def translate(varName: String) = {
+        //var components = Array[String]()
 
-    def isSplitChar(c: Char)    = c.isUpper || c == '_' || c == '-' || c == ' '
-    def isUpcasePrefix(c: Char) = c.isUpper || c.isDigit
-
-    def translate(varName: String) = {
-      //var components = Array[String]()
-
-      def wikiNameComponents: List[String] = {
-        findWikiNameComponent(0)
-      }
-
-      def findWikiNameComponent(index: Int): List[String] = {
-        val len = varName.length
-
-        def skipUpcasePrefix(i: Int): Int = {
-          if (i < len && isUpcasePrefix(varName(i))) {
-            skipUpcasePrefix(i + 1)
-          } else {
-            i
-          }
+        def wikiNameComponents: List[String] = {
+          findWikiNameComponent(0)
         }
 
-        def parseWikiComponent(i: Int): Int =
-          if (i < len && !isSplitChar(varName(i))) {
-            parseWikiComponent(i + 1)
-          } else {
-            i
-          }
+        def findWikiNameComponent(index: Int): List[String] = {
+          val len = varName.length
 
-        val start  = index
-        var cursor = index
-        if (cursor >= len) {
-          Nil
-        } else {
-          cursor = skipUpcasePrefix(cursor)
-          // Upcase prefix length is longer than or equals to 2
-          if (cursor - start >= 2) {
-            if (start == 0 && varName(cursor).isLower) {
-              cursor -= 1
-            }
-            varName.substring(start, cursor) :: findWikiNameComponent(cursor)
-          } else {
-            cursor = parseWikiComponent(cursor)
-            if (start < cursor) {
-              varName.substring(start, cursor).toLowerCase() :: findWikiNameComponent(cursor)
+          def skipUpcasePrefix(i: Int): Int = {
+            if (i < len && isUpcasePrefix(varName(i))) {
+              skipUpcasePrefix(i + 1)
             } else {
-              findWikiNameComponent(cursor + 1)
+              i
+            }
+          }
+
+          def parseWikiComponent(i: Int): Int =
+            if (i < len && !isSplitChar(varName(i))) {
+              parseWikiComponent(i + 1)
+            } else {
+              i
+            }
+
+          val start  = index
+          var cursor = index
+          if (cursor >= len) {
+            Nil
+          } else {
+            cursor = skipUpcasePrefix(cursor)
+            // Upcase prefix length is longer than or equals to 2
+            if (cursor - start >= 2) {
+              if (start == 0 && varName(cursor).isLower) {
+                cursor -= 1
+              }
+              varName.substring(start, cursor) :: findWikiNameComponent(cursor)
+            } else {
+              cursor = parseWikiComponent(cursor)
+              if (start < cursor) {
+                varName.substring(start, cursor).toLowerCase() :: findWikiNameComponent(cursor)
+              } else {
+                findWikiNameComponent(cursor + 1)
+              }
             }
           }
         }
+
+        val components = wikiNameComponents
+        val nName      = components.mkString(" ")
+        nName
       }
 
-      val components = wikiNameComponents
-      val nName      = components.mkString(" ")
-      nName
+      naturalNameTable.getOrElseUpdate(varName, translate(varName))
     }
-
-    naturalNameTable.getOrElseUpdate(varName, translate(varName))
   }
 
 }
@@ -135,10 +136,9 @@ class CName(val canonicalName: String, val naturalName: String) extends Comparab
 
   override def hashCode = canonicalName.hashCode()
   override def equals(other: Any) = {
-    if (other.isInstanceOf[CName]) {
-      canonicalName.equals(other.asInstanceOf[CName].canonicalName)
-    } else {
-      false
+    other match {
+      case o: CName if canonicalName.equals(o.canonicalName) => true
+      case _                                                 => false
     }
   }
 
