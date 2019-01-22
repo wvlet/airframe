2590bf6ef9b6bffc6b871ad201e5073e5c6c088e
Taro L. Saito <leo@xerial.org>
2019-01-21 16:26:59 -0800

    Change airframe-control to a Scala JVM project

--- airframe-control/src/main/scala/wvlet/airframe/control/CommandLineTokenizer.scala
+++ airframe-control/src/main/scala/wvlet/airframe/control/CommandLineTokenizer.scala
@@ -1,11 +1,9 @@
 /*
- * Copyright 2012 Taro L. Saito
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,24 +11,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package wvlet.airframe.control
-
 import wvlet.log.LogSupport
 
 import scala.util.parsing.combinator.RegexParsers
 
-//--------------------------------------
-//
-// CommandLineTokenizer.scala
-// Since: 2012/07/17 18:38
-//
-//--------------------------------------
-
 /**
   * Tokenize single string representations of command line arguments into Array[String]
-  *
-  * @author Taro L. Saito
   */
 object CommandLineTokenizer extends RegexParsers with LogSupport {
 
