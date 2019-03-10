281cb1487bc62f9f61fe9a9efa70d2444ee0bb5c
Taro L. Saito <leo@xerial.org>
2019-03-10 09:57:22 -0700

    Rename wvlet.msgframe.sql -> wvlet.airframe.sql

---
+++ airframe-sql/src/test/scala/wvlet/airframe/sql/SQLBenchmark.scala
@@ -0,0 +1,102 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package wvlet.airframe.sql
+
+import java.io.File
+
+import wvlet.airframe.codec.MessageCodecFactory
+import wvlet.airframe.config.YamlReader
+import wvlet.log.io.IOUtil
+
+/**
+  *
+  */
+object SQLBenchmark {
+
+  case class TestQuery(sql: String, path: Option[String]) {
+    override def toString = s"${name}:\n${sql}"
+    def name              = path.getOrElse(s"${sql.substring(0, 20.min(sql.length))}")
+  }
+
+  private val RESOURCE_PATH = "airframe-sql/src/test/resources/wvlet/airframe/sql"
+
+  private def readSQLFromYaml(path: String): Seq[TestQuery] = {
+    if (IOUtil.findPath(path).isEmpty) {
+      Seq.empty
+    } else {
+      val yaml = YamlReader.loadYamlList(path)
+
+      yaml
+        .map { y =>
+          val msgpack = YamlReader.toMsgPack(y)
+          val codec   = MessageCodecFactory.defaultFactory.of[TestQuery]
+          codec.unpackMsgPack(msgpack).map { x =>
+            x
+          }
+        }
+        .filter(_.isDefined)
+        .flatten
+    }
+  }
+
+  def allQueries: Seq[TestQuery] = {
+    standardQueries ++ tpcH ++ tpcDS
+  }
+
+  def standardQueries: Seq[TestQuery] = {
+    selection ++ ddl
+  }
+
+  lazy val selection: Seq[TestQuery] = {
+    readSQLFromYaml(s"${RESOURCE_PATH}/standard/queries.yml")
+  }
+
+  lazy val ddl: Seq[TestQuery] = {
+    readSQLFromYaml(s"${RESOURCE_PATH}/standard/ddl.yml")
+  }
+
+  private def readTestQueries(dir: File): Seq[TestQuery] = {
+    for (f <- dir.listFiles() if f.getName.endsWith(".sql")) yield {
+      TestQuery(IOUtil.readAsString(f.getPath), Some(f.getName))
+    }
+  }
+
+  lazy val tpcDS: Seq[TestQuery] = {
+    val dir = new File(s"${RESOURCE_PATH}/tpc-ds")
+    readTestQueries(dir).filter { x =>
+      // TODO support rollup operator
+      !x.sql.toLowerCase.contains("rollup")
+    }
+  }
+
+  def tpcDS_(q: String): TestQuery = {
+    val f = new File(s"${RESOURCE_PATH}/tpc-ds/${q}.sql")
+    TestQuery(IOUtil.readAsString(f.getPath), Some(f.getName))
+  }
+
+  lazy val tpcH: Seq[TestQuery] = {
+    val dir = new File(s"${RESOURCE_PATH}/tpc-h")
+    readTestQueries(dir)
+  }
+
+  lazy val hive: Seq[TestQuery] = {
+    readSQLFromYaml(s"${RESOURCE_PATH}/standard/hive-queries.yml")
+  }
+
+  lazy val privateQueries: Seq[TestQuery] = {
+    readSQLFromYaml(s"${RESOURCE_PATH}/private/examples.yml")
+  }
+
+}
---
+++ airframe-sql/src/test/scala/wvlet/airframe/sql/analyzer/QuerySignatureTest.scala
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package wvlet.airframe.sql.analyzer
+
+import wvlet.airframe.AirframeSpec
+import wvlet.airframe.sql.SQLBenchmark
+import wvlet.airframe.sql.parser.SQLParser
+
+/**
+  *
+  */
+class QuerySignatureTest extends AirframeSpec {
+
+  "Find input/output tables" in {
+    SQLBenchmark.allQueries.foreach { sql =>
+      val g = TableGraph.of(sql.sql)
+      debug(g)
+    }
+  }
+
+  "Generate signature" in {
+    SQLBenchmark.allQueries.foreach { sql =>
+      val s = QuerySignature.of(sql.sql)
+      debug(s)
+    }
+  }
+
+  "parse q72.sql" taggedAs (working) in {
+    val sql = SQLBenchmark.tpcDS_("q72")
+    debug(sql)
+    val p = SQLParser.parse(sql.sql)
+    debug(p)
+    val sig = QuerySignature.of(sql.sql)
+    debug(sig)
+  }
+}
