c5a3171a859f4aa1f77424489fd929b2a0770fda
Naoki Takezoe <takezoe@gmail.com>
2019-07-07 04:23:24 +0900

    DSL for JSON extraction

--- airframe-json/src/main/scala/wvlet/airframe/json/package.scala
+++ airframe-json/src/main/scala/wvlet/airframe/json/package.scala
@@ -12,7 +12,16 @@
  * limitations under the License.
  */
 package wvlet.airframe
-import wvlet.airframe.json.JSON.JSONValue
+import wvlet.airframe.json.JSON.{
+  JSONArray,
+  JSONBoolean,
+  JSONDouble,
+  JSONLong,
+  JSONNull,
+  JSONObject,
+  JSONString,
+  JSONValue
+}
 
 /**
   *
@@ -23,5 +32,49 @@ package object json {
 
   implicit class RichJson(val json: Json) extends AnyVal {
     def toJSONValue: JSONValue = JSON.parseAny(json)
+  }
+
+  implicit class JSONValueOps(jsonValue: JSONValue) {
+    def \(name: String): Seq[JSONValue] = {
+      jsonValue match {
+        case jsonObject: JSONObject =>
+          jsonObject.v.collect {
+            case (key, value) if key == name =>
+              value
+          }
+        case jsonArray: JSONArray =>
+          jsonArray.v.flatMap {
+            case value =>
+              value \ name
+          }
+        case _ => Nil
+      }
+    }
+
+    def value: Any = {
+      jsonValue match {
+        case JSONNull       => null
+        case JSONDouble(x)  => x
+        case JSONLong(x)    => x
+        case JSONString(x)  => x
+        case JSONBoolean(x) => x
+        case JSONArray(x)   => x.map(_.value)
+        case JSONObject(x)  => x.map(x => (x._1, x._2.value)).toMap
+      }
+    }
+  }
+
+  implicit class JSONValueSeqOps(jsonValues: Seq[JSONValue]) {
+    def \(name: String): Seq[JSONValue] = {
+      jsonValues.flatMap { jsonValue =>
+        jsonValue \ name
+      }
+    }
+
+    def values: Seq[Any] = {
+      jsonValues.map { jsonValue =>
+        jsonValue.value
+      }
+    }
   }
 }
