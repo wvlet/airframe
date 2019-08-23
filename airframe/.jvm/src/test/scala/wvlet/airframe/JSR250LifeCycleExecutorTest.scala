7bdf1eae2164fc3b29e17e3e0422544478775b4a
Taro L. Saito <leo@xerial.org>
2019-08-23 11:43:19 -0700

    Add test for checking lifecycle hook overwrite

--- airframe/.jvm/src/test/scala/wvlet/airframe/JSR250LifeCycleExecutorTest.scala
+++ airframe/.jvm/src/test/scala/wvlet/airframe/JSR250LifeCycleExecutorTest.scala
@@ -17,8 +17,9 @@ import javax.annotation.{PostConstruct, PreDestroy}
 import wvlet.airspec.AirSpec
 
 class JSR250Test {
-  var initialized = false
-  var stopped     = false
+  var initialized      = false
+  var stopped          = false
+  var stoppedCallCount = 0
 
   @PostConstruct
   def init: Unit = {
@@ -28,10 +29,22 @@ class JSR250Test {
   @PreDestroy
   def stop: Unit = {
     stopped = true
+    stoppedCallCount += 1
   }
 }
 
 class InheritedHookTest extends JSR250Test
+
+class HookOverrideTest extends JSR250Test {
+  var parentDestoyHookIsCalled = false
+
+  @PreDestroy
+  override def stop: Unit = {
+    stopped = true
+    parentDestoyHookIsCalled = true
+    stoppedCallCount += 1
+  }
+}
 
 /**
   *
@@ -58,4 +71,19 @@ class JSR250LifeCycleExecutorTest extends AirSpec {
     t.initialized shouldBe true
     t.stopped shouldBe true
   }
+
+  def `do not call overwritten hooks`: Unit = {
+    val s = newSilentDesign.newSession
+
+    val t = s.build[HookOverrideTest]
+    t.initialized shouldBe true
+    t.stopped shouldBe false
+    t.parentDestoyHookIsCalled shouldBe false
+    s.start {}
+    t.initialized shouldBe true
+    t.stopped shouldBe true
+    t.stoppedCallCount shouldBe 1
+    t.parentDestoyHookIsCalled shouldBe true
+  }
+
 }
