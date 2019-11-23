2aabbecf77efbd86ce785499a60048d1b604db35
Taro L. Saito <leo@xerial.org>
2019-11-22 16:46:11 -0800

    Reformat

--- airframe-http/src/main/scala/wvlet/airframe/http/router/RouteMatcher.scala
+++ airframe-http/src/main/scala/wvlet/airframe/http/router/RouteMatcher.scala
@@ -71,7 +71,7 @@ object RouteMatcher extends LogSupport {
             throw new IllegalArgumentException(
               s"Found multiple matching routes: ${state.map(_.route).flatten.map(p => s"${p.path}").mkString(", ")} "
             )
-        }
+          }
       )
 
     def findRoute[Req](request: Req)(implicit tp: HttpRequestAdapter[Req]): Option[RouteMatch] = {
