db24f2a7f2e68bc0f27cbab753ca9d7d2607dbf3
Taro L. Saito <leo@xerial.org>
2019-12-10 23:26:48 -0800

    Fix wording

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -113,50 +113,50 @@ class Index extends React.Component {
             <Block layout="threeColumn">
                 {[
                     {
-                        content: 'Airframe has redesigned everything for Scala and can be a replacement of the commonly-used libraries in Java, such as Jackson, Google Guava, Guice, slf4j, etc. This is useful for minimizing the dependency hell of these libraries. Airframe also supports Scala.js as most of the components are written purely in Scala.',
+                        content: 'Airframe has redesigned everything for Scala to be a replacement of the commonly-used libraries in Java world, such as Jackson, Google Guava, Guice, slf4j, etc. This is useful for minimizing the dependency hell of these libraries. Airframe also supports Scala.js as most of the components are purely written in Scala.',
                         image: `${baseUrl}img/features/scala-logo-red-spiral-dark.png`,
                         imageAlign: 'top',
                         title: 'Designed for Scala',
                     },
                     {
                         content:
-                            'Airframe uses [MessagePack-based schema-on-read codec](docs/airframe-codec) for fast and compact object serialization. It can be used for efficient HTTP server-client communication, and automatically resolves differences between data types (e.g., integers and strings.). Serialization with [JSON](docs/airframe-json) is also supported. ',
+                            'Airframe uses [MessagePack-based schema-on-read codec](docs/airframe-codec) for fast and compact object serialization. It can be used for efficient HTTP server-client communication and for automatically resolving differences between data types (e.g., integers and strings.) Serialization with [JSON](docs/airframe-json) is also supported.',
                         image: `${baseUrl}img/features/msgpack.png`,
                         imageAlign: 'top',
                         title: 'MessagePack Serialization',
                     },
                     {
-                        content: 'With [dependency injection (DI)](docs/airframe) of Airframe, building services with hundreds of module classes becomes easy and manageable. Airframe DI will build complex objects on your behalf by following [your custom design](docs/airframe#design) and properly [start and shutdown your services in FILO order](docs/airframe#life-cycle).',
+                        content: 'With [dependency injection (DI)](docs/airframe) of Airframe, building services with hundreds of module classes becomes easy and manageable. Airframe DI will build complex objects on your behalf based on the [design](docs/airframe#design) and properly [start and shutdown your services in the FILO order](docs/airframe#life-cycle).',
                         image: `${baseUrl}img/features/di.png`,
                         imageAlign: 'top',
                         title: 'Dependency Injection'
                     },
                     {
-                        content: 'Logging is an essential tool for debugging applications. [airframe-log](docs/airframe-log) helps displaying logs with fancy ANSI colors enriched with the source code locations.',
+                        content: 'Logging is an essential tool for debugging applications. [airframe-log](docs/airframe-log) helps displaying logs in fancy ANSI colors enriched with the source code locations.',
                         image: `${baseUrl}img/airframe-log/demo.png`,
                         imageAlign: 'top',
                         title: 'Colorful Source Code Logging',
                     },
                     {
-                        content: '[airframe-config](docs/airframe-config) supports YAML-based application configurations and provides immutable config objects that can be injected through DI. Partially overriding configutations is also supported.',
+                        content: '[airframe-config](docs/airframe-config) supports YAML-based application configurations and provides immutable config objects that can be injected through DI. Partially overriding configurations is also supported.',
                         image: `${baseUrl}img/immutable_config.png`,
                         imageAlign: 'top',
                         title: 'Application Config Flow',
                     },
                     {
-                        content: 'Need a way to parse command line options? [airframe-launcher](docs/airframe-launcher) is a handy command line parser that can be used to call Scala functions from the command line arguments.',
+                        content: 'Need a way to parse command line options? [airframe-launcher](docs/airframe-launcher) will turn your Scala functions into command-line programs which can read complex command-line arguments.',
                         image: `${baseUrl}/img/features/launcher.png`,
                         imageAlign: 'top',
                         title: 'Command-Line Parser'
                     },
                     {
-                        content: '[AirSpec](docs/airspec) is a simple unit testing framework for Scala and Scala.js. You can uses public functions in your classes as test cases. No need to remember complex DSLs to start writing tests in Scala.',
+                        content: '[AirSpec](docs/airspec) is a simple unit testing framework for Scala and Scala.js. You can use public methods in your classes as test cases. No need to remember complex DSLs for writing tests in Scala.',
                         image: `${baseUrl}/img/features/airspec.png`,
                         imageAlign: 'top',
                         title: 'Simple Testing Framework'
                     },
                     {
-                        content: 'Retrying HTTP requests for API calls is an essential technique for connecting microservices. [airframe-control](docs/airframe-control) provides essential tools for making your requests reliable with exponential backoff retry, jitter, circuit-breaker, rate control, etc.',
+                        content: 'Retrying HTTP requests for API calls is an essential technique for connecting microservices. [airframe-control](docs/airframe-control) will provide essential tools for making your requests reliable with exponential backoff retry, jitter, circuit-breaker, rate control, etc.',
                         image: `${baseUrl}/img/features/undraw_process_e90d.svg`,
                         imageAlign: 'top',
                         title: 'Retry, Rate Control'
@@ -168,13 +168,13 @@ class Index extends React.Component {
                         title: 'Web Service IDL in Scala'
                     },
                     {
-                        content: "Developing web applications needs efficient HTTP clients and mock servers even if the network is disconnected. With [airframe-http-recorder](docs/airframe-http-recorder) you can record real server responses and replay them for running unit tests even if the servers are offline.",
+                        content: "Web application development often requires mock web servers. With [airframe-http-recorder](docs/airframe-http-recorder), you can record and replay the real server responses for running unit tests even if the servers are offline or unreachable from CI environments.",
                         image: `${baseUrl}/img/features/undraw_server_down_s4lk.svg`,
                         imageAlign: 'top',
                         title: 'HTTP Client and Recorders',
                     },
                     {
-                        content: `[airframe-fluentd](docs/airframe-fluentd) supports logging your metrics to fluentd. Collecting metrics is much easier with the type-safe logging of airframe-fluentd.`,
+                        content: `[airframe-fluentd](docs/airframe-fluentd) supports logging your metrics to fluentd in a type-safe manner. You just need to send your case classes as metrics for fluentd.`,
                         image: `${baseUrl}/img/features/Fluentd_square.svg`,
                         imageAlign: 'top',
                         title: 'Fluentd Logging',
