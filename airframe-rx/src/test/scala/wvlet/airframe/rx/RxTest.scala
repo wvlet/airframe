c61293d85ce90f27306e891656a31b624542587f
Taro L. Saito <leo@xerial.org>
2019-12-25 23:50:38 -0800

    add test

---
+++ airframe-rx/src/test/scala/wvlet/airframe/rx/RxTest.scala
@@ -0,0 +1,13 @@
+package wvlet.airframe.rx
+import wvlet.airspec._
+
+class RxTest extends AirSpec {
+  def `create a new variable`: Unit = {
+    val v  = Rx(1)
+    val rx = v.map(x => s"count: ${x}")
+
+    rx.subscribe(x => logger.info(x))
+
+    v.update(2)
+  }
+}
