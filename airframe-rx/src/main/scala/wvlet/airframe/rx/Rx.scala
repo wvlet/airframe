6b1479fc6090acc5c6a7e156d6f0dbd61ae2013c
Taro L. Saito <leo@xerial.org>
2019-12-25 22:57:01 -0800

    Add UniaryRx

--- airframe-rx/src/main/scala/wvlet/airframe/rx/Rx.scala
+++ airframe-rx/src/main/scala/wvlet/airframe/rx/Rx.scala
@@ -22,10 +22,11 @@ trait Rx[A] {
   private[rx] def addDownstream[B](rx: Rx[B]): Rx[B]
   private[rx] def addSubscriber(s:Subscriber[A]): Unit
 
-  def map[B](f: A => B): Rx[B] = MapOp[A, B](this, f))
+  def map[B](f: A => B): Rx[B] = MapOp[A, B](this, f)
   def flatMap[B](f: A => Rx[B]): Rx[B] = FlatMapOp(this, f)
   def withName(name:String): Rx[A] = NamedOp(this, name)
 
+  // The parent operators of this Rx[A]
   def parents: Seq[Rx[_]]
 
   def subscribe[U](subscriber: A => U): Unit = {
@@ -44,21 +45,37 @@ object Rx {
 
   private[rx] abstract class RxBase[A] extends Rx[A] {
     private[rx] var downStream: List[Rx[_]] = List.empty
+    private[rx] var subscribers: List[Subscriber[A]] = List.empty
+
     private[rx] override def addDownstream[B](rx: Rx[B]): Rx[B] = {
       synchronized {
         downStream = rx :: downStream
         rx
       }
     }
+
+    private[rx] override def addSubscriber(s:Subscriber[A]): Unit = {
+      synchronized {
+        subscribers = s :: subscribers
+      }
+    }
   }
 
+  abstract class UnaryRx[A] extends RxBase[A] {
+    def input: Rx[_]
+    override def parents: Seq[Rx[_]] = Seq(input)
+  }
 
-  case class SingleOp[A](v:A) extends RxBase[A]
-  case class MapOp[A, B](ref: Rx[A], f: A => B) extends RxBase[B]
-  case class FlatMapOp[A, B](ref: Rx[A], f: A => Rx[B]) extends RxBase[B]
-  case class NamedOp[A](ref: Rx[A], name:String) extends RxBase[A]
+  case class SingleOp[A](v:A) extends RxBase[A] {
+    override def parents: Seq[Rx[_]] = Seq.empty
+  }
+  case class MapOp[A, B](input: Rx[A], f: A => B) extends UnaryRx[B]
+  case class FlatMapOp[A, B](input: Rx[A], f: A => Rx[B]) extends UnaryRx[B]
+  case class NamedOp[A](input: Rx[A], name:String) extends UnaryRx[A]
 
   class RxVar[A](private[rx] var currentValue: A) extends RxBase[A] {
+    override def parents: Seq[Rx[_]] = Seq.empty
+
     def :=(newValue: A): Unit = update(newValue)
     def update(newValue: A): Unit = {
       if (currentValue != newValue) {
--- build.sbt
+++ build.sbt
@@ -295,7 +295,7 @@ lazy val airframeMacros =
     )
     .jsSettings(jsBuildSettings)
 
-laAzy val airframeMacrosJVM = airframeMacros.jvm
+lazy val airframeMacrosJVM = airframeMacros.jvm
 lazy val airframeMacrosJS  = airframeMacros.js
 
 // To use airframe in other airframe modules, we need to reference airframeMacros project using the internal scope
