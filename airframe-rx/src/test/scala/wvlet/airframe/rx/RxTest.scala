7835cc90bbeb440d76f7a6556f9b5f7c24cfa986
Taro L. Saito <leo@xerial.org>
2019-12-26 22:28:25 -0800

    Add log message

--- airframe-rx/src/test/scala/wvlet/airframe/rx/RxTest.scala
+++ airframe-rx/src/test/scala/wvlet/airframe/rx/RxTest.scala
@@ -4,10 +4,9 @@ import wvlet.airspec._
 class RxTest extends AirSpec {
   def `create a new variable`: Unit = {
     val v  = Rx(1)
-    val rx = v.map(x => s"count: ${x}")
+    val rx = v.map(x => s"count: ${x}").withName("sample rx")
 
     rx.subscribe(x => logger.info(x))
-
     v.update(2)
   }
 }
