f0cfed6489362d737da75b1f50dc61ae891ece59
Taro L. Saito <leo@xerial.org>
2019-12-30 11:07:03 -0800

    add config

--- airframe-control/src/main/scala/wvlet/airframe/control/CircuitBreaker.scala
+++ airframe-control/src/main/scala/wvlet/airframe/control/CircuitBreaker.scala
@@ -15,29 +15,55 @@ package wvlet.airframe.control
 
 import java.util.concurrent.atomic.AtomicReference
 
-import wvlet.airframe.control.CircuitBreaker.CircuitBreakerState
 import wvlet.airframe.control.ResultClass.{Failed, Succeeded}
 import wvlet.airframe.control.Retry.RetryableFailure
 
 import scala.util.{Failure, Success, Try}
+import wvlet.log.LogSupport
+
+case class CircuitBreakerConfig(
+    name: String = "default",
+    healthCheckPolicy: HealthCheckPolicy = HealthCheckPolicy.markDeadOnConsecutiveFailures(3),
+    resultClassifier: Any => ResultClass = ResultClass.ALWAYS_SUCCEED,
+    errorClassifier: Throwable => ResultClass = ResultClass.ALWAYS_RETRY,
+    onOpen: CircuitBreakerContext => Unit = CircuitBreaker.throwOpenException,
+    onStateChange: CircuitBreakerContext => Unit = CircuitBreaker.reportStateChange
+) {
+  def withName(name: String): CircuitBreakerConfig = {
+    this.copy(name = name)
+  }
+  def withHealthCheckPolicy(healthCheckPolicy: HealthCheckPolicy): CircuitBreakerConfig = {
+    this.copy(healthCheckPolicy = healthCheckPolicy)
+  }
+}
 
 /**
   *
   */
-object CircuitBreaker {
+object CircuitBreaker extends LogSupport {
 
-  def apply(): CircuitBreaker = new CircuitBreaker("circuit-breaker")
+  /**
+    * Thrown when the circuit breaker is open
+    */
+  case class CircuitBreakerOpenException(context: CircuitBreakerContext) extends Exception
+
+  def default: CircuitBreakerConfig = CircuitBreakerConfig()
+  def apply(config: CircuitBreakerConfig): CircuitBreaker = {
+    new CircuitBreaker(config = config)
+  }
 
   sealed trait CircuitBreakerState
   case object OPEN      extends CircuitBreakerState
   case object HALF_OPEN extends CircuitBreakerState
   case object CLOSED    extends CircuitBreakerState
 
-  private def throwOpenException: CircuitBreakerContext => Unit = { ctx: CircuitBreakerContext =>
+  private[control] def throwOpenException: CircuitBreakerContext => Unit = { ctx: CircuitBreakerContext =>
     throw CircuitBreakerOpenException(ctx)
   }
 
-  case class CircuitBreakerOpenException(context: CircuitBreakerContext) extends Exception
+  private[control] def reportStateChange = { ctx: CircuitBreakerContext =>
+    info(s"CircuitBreaker(namd:${ctx.name}) state is changed to ${ctx.state}")
+  }
 }
 
 import CircuitBreaker._
@@ -50,36 +76,27 @@ trait CircuitBreakerContext {
 
 case class CircuitBreaker(
     // The name of circuit
-    name: String,
+    config: CircuitBreakerConfig,
     lastFailure: Option[Throwable] = None,
-    healthCheckPolicy: HealthCheckPolicy,
-    resultClassifier: Any => ResultClass = ResultClass.ALWAYS_SUCCEED,
-    errorClassifier: Throwable => ResultClass = ResultClass.ALWAYS_RETRY,
-    onOpen: CircuitBreakerContext => Unit = CircuitBreaker.throwOpenException,
-    onStateChange: CircuitBreakerContext => Unit,
-    private val currentState: AtomicReference[CircuitBreakerState] =
-      new AtomicReference[CircuitBreakerState](CircuitBreaker.CLOSED)
+    private val currentState: AtomicReference[CircuitBreakerState] = new AtomicReference(CircuitBreaker.CLOSED)
 ) extends CircuitBreakerContext {
 
+  def name: String               = config.name
   def state: CircuitBreakerState = currentState.get()
-
-  def withName(name: String): CircuitBreaker = {
-    this.copy(name = name)
-  }
-  def withHealthCheckPolicy(healthCheckPolicy: HealthCheckPolicy): CircuitBreaker = {
-    this.copy(healthCheckPolicy = healthCheckPolicy)
-  }
 
   def open: this.type = {
     currentState.set(OPEN)
+    config.onStateChange(this)
     this
   }
   def halfOpen: this.type = {
     currentState.set(HALF_OPEN)
+    config.onStateChange(this)
     this
   }
   def close: this.type = {
     currentState.set(CLOSED)
+    config.onStateChange(this)
     this
   }
 
@@ -89,29 +106,29 @@ case class CircuitBreaker(
 
   def run[A](body: => A): Unit = {
     if (!isConnected) {
-      onOpen(this)
+      config.onOpen(this)
     } else {
       val result = Try(body)
       val resultClass = result match {
-        case Success(x) => resultClassifier(x)
+        case Success(x) => config.resultClassifier(x)
         case Failure(RetryableFailure(e)) =>
           ResultClass.retryableFailure(e)
         case Failure(e) =>
-          errorClassifier(e)
+          config.errorClassifier(e)
       }
       resultClass match {
         case Succeeded =>
-          healthCheckPolicy.recovered
+          config.healthCheckPolicy.recovered
           currentState.get() match {
             case HALF_OPEN | CLOSED =>
-              healthCheckPolicy.recovered
+              config.healthCheckPolicy.recovered
               close
             case _ =>
           }
-          healthCheckPolicy.recordSuccess
+          config.healthCheckPolicy.recordSuccess
           result.get
         case Failed(retryable, cause, extraWait) =>
-          healthCheckPolicy.recordFailure
+          config.healthCheckPolicy.recordFailure
           throw cause
       }
     }
--- airframe-control/src/test/scala/wvlet/airframe/control/CircuitBreakerTest.scala
+++ airframe-control/src/test/scala/wvlet/airframe/control/CircuitBreakerTest.scala
@@ -1,12 +1,24 @@
 package wvlet.airframe.control
 
 import wvlet.airspec._
-import wvlet.airframe.control.CircuitBreaker
 
 class CircuitBreakerTest extends AirSpec {
 
-  def `throw exception on open`: Unit = {
-    val cb = CircuitBreaker()
+  def `support changing states`: Unit = {
+    val cb = CircuitBreaker.default
     cb.state shouldBe CircuitBreaker.CLOSED
+    cb.isConnected shouldBe true
+
+    cb.open
+    cb.state shouldBe CircuitBreaker.OPEN
+    cb.isConnected shouldBe false
+
+    cb.halfOpen
+    cb.state shouldBe CircuitBreaker.HALF_OPEN
+    cb.isConnected shouldBe false
+
+    cb.close
+    cb.state shouldBe CircuitBreaker.CLOSED
+    cb.isConnected shouldBe true
   }
 }
