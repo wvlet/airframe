79a2028db27862ea49e144f189b3d294c2e24e76
Taro L. Saito <leo@xerial.org>
2020-05-27 21:36:23 -0700

    add test

--- airframe-metrics/shared/src/test/scala/wvlet/airframe/metrics/CountTest.scala
+++ airframe-metrics/shared/src/test/scala/wvlet/airframe/metrics/CountTest.scala
@@ -27,21 +27,22 @@ class CountTest extends AirSpec {
 
   test("parse count strings") {
     check("1M", Count(1000000L, Count.MILLION), "1M")
+    check("123M", Count(123000000L, Count.MILLION), "123M")
+
     check("123", Count(123, Count.ONE), "123")
     check("1234", Count(1234, Count.ONE), "1,234")
-    check("1,234", Count(1234, Count.ONE), "1,234")
-    check("1.2K", Count(1200, Count.THOUSAND), "1.20K")
     check("2000", Count(2000, Count.ONE), "2,000")
+
     check("2K", Count(2000, Count.THOUSAND), "2K")
+    check("10B", Count(10000000000L, Count.BILLION), "10B")
+    check("1.23B", Count(1230000000L, Count.BILLION), "1.23B")
   }
 
   test("parse count strings with fraction") {
-    val c = Count("1.2M")
-    c shouldBe Count(1200000L, Count.MILLION)
-    c.toString shouldBe "1.20M"
-
-    val c2 = Count("123M")
-    c2 shouldBe Count(123000000L, Count.MILLION)
-    c2.toString shouldBe "123M"
+    check("1.2K", Count(1200, Count.THOUSAND), "1.20K")
+    check("1.2M", Count(1200000L, Count.MILLION), "1.20M")
+    check("1.234M", Count(1234000L, Count.MILLION), "1.23M")
+    check("1.235M", Count(1235000L, Count.MILLION), "1.24M")
+    check("123.4M", Count(123400000L, Count.MILLION), "123.40M")
   }
 }
