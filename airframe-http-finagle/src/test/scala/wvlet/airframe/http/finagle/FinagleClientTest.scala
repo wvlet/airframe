c2bd13f3ba7ea99dc0b337f2bc96a8f583dbfd5f
Taro L. Saito <leo@xerial.org>
2020-06-17 15:10:13 -0700

    Simplify the test design

--- airframe-http-finagle/src/test/scala/wvlet/airframe/http/finagle/FinagleClientTest.scala
+++ airframe-http-finagle/src/test/scala/wvlet/airframe/http/finagle/FinagleClientTest.scala
@@ -15,6 +15,7 @@ package wvlet.airframe.http.finagle
 
 import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.util.Await
+import wvlet.airframe.Design
 import wvlet.airframe.control.Control.withResource
 import wvlet.airframe.http._
 import wvlet.airspec.AirSpec
@@ -40,6 +41,11 @@ trait FinagleClientTestApi extends LogSupport {
   @Endpoint(method = HttpMethod.GET, path = "/user/:id")
   def get(id: Int, request: Request): User = {
     User(id, "leo", getRequestId(request))
+  }
+
+  @Endpoint(method = HttpMethod.GET, path = "/user/:id/invalid")
+  def getInvalidUser(id: Int, request: Request): HttpMessage.Response = {
+    Http.response(HttpStatus.Ok_200).withJson("""{"id":"abc","user":"NA","requestId":"xxx"}""")
   }
 
   @Endpoint(method = HttpMethod.GET, path = "/user/info")
@@ -99,137 +105,139 @@ trait FinagleClientTestApi extends LogSupport {
 /**
   */
 class FinagleClientTest extends AirSpec {
-  val r = Router.add[FinagleClientTestApi]
-  val d = finagleDefaultDesign
-    .bind[FinagleServerConfig].toInstance(
-      FinagleServerConfig(name = "test-server", router = r)
-    )
-    .noLifeCycleLogging
+  private val r = Router.add[FinagleClientTestApi]
 
-  def `create client`: Unit = {
+  override protected def design: Design =
+    Finagle.server
+      .withName("test-server")
+      .withRouter(r)
+      .designWithSyncClient
+
+  def `convert HTTP responses into objects`(client: FinagleSyncClient): Unit = {
     def addRequestId(request: Request): Request = {
       request.headerMap.put("X-Request-Id", "10")
       request
     }
+    // Sending an implementation specific Request type
+    val ret = client.send(Request("/")).contentString
+    ret shouldBe "Ok"
 
-    d.build[FinagleServer] { server =>
-      withResource(FinagleClient.newSyncClient(server.localAddress)) { client =>
-        // Sending an implementation specific Request type
-        val ret = client.send(Request("/")).contentString
-        ret shouldBe "Ok"
+    // Using HTTP request wrappers
+    client.get[User]("/user/1") shouldBe User(1, "leo", "N/A")
+    client.getResource[UserRequest, User]("/user/info", UserRequest(2, "kai")) shouldBe User(2, "kai", "N/A")
+    client.getResource[UserRequest, User]("/user/info2", UserRequest(2, "kai")) shouldBe User(2, "kai", "N/A")
+    client.list[Seq[User]]("/user") shouldBe Seq(User(1, "leo", "N/A"))
 
-        // Using HTTP request wrappers
-        client.get[User]("/user/1") shouldBe User(1, "leo", "N/A")
-        client.getResource[UserRequest, User]("/user/info", UserRequest(2, "kai")) shouldBe User(2, "kai", "N/A")
-        client.getResource[UserRequest, User]("/user/info2", UserRequest(2, "kai")) shouldBe User(2, "kai", "N/A")
-        client.list[Seq[User]]("/user") shouldBe Seq(User(1, "leo", "N/A"))
+    client.post[User]("/user", User(2, "yui", "N/A")) shouldBe User(2, "yui", "N/A")
+    client.postOps[User, User]("/user", User(2, "yui", "N/A")) shouldBe User(2, "yui", "N/A")
 
-        client.post[User]("/user", User(2, "yui", "N/A")) shouldBe User(2, "yui", "N/A")
-        client.postOps[User, User]("/user", User(2, "yui", "N/A")) shouldBe User(2, "yui", "N/A")
+    client.put[User]("/user", User(10, "aina", "N/A")) shouldBe User(10, "aina", "N/A")
+    client.putOps[User, User]("/user", User(10, "aina", "N/A")) shouldBe User(10, "aina", "N/A")
 
-        client.put[User]("/user", User(10, "aina", "N/A")) shouldBe User(10, "aina", "N/A")
-        client.putOps[User, User]("/user", User(10, "aina", "N/A")) shouldBe User(10, "aina", "N/A")
+    client.patch[User]("/user", User(20, "joy", "N/A")) shouldBe User(20, "joy", "N/A")
+    client.patchOps[User, User]("/user", User(20, "joy", "N/A")) shouldBe User(20, "joy", "N/A")
 
-        client.patch[User]("/user", User(20, "joy", "N/A")) shouldBe User(20, "joy", "N/A")
-        client.patchOps[User, User]("/user", User(20, "joy", "N/A")) shouldBe User(20, "joy", "N/A")
+    client.delete[User]("/user/1") shouldBe User(1, "xxx", "N/A")
+    client.deleteOps[DeleteRequestBody, User]("/user/1", DeleteRequestBody(true)) shouldBe User(1, "xxx", "N/A")
 
-        client.delete[User]("/user/1") shouldBe User(1, "xxx", "N/A")
-        client.deleteOps[DeleteRequestBody, User]("/user/1", DeleteRequestBody(true)) shouldBe User(1, "xxx", "N/A")
+    // application/x-msgpack test
+    client.get[User]("/user/1", { r => r.accept = "application/x-msgpack"; r }) shouldBe User(1, "leo", "N/A")
+    client.post[User]("/user", User(2, "yui", "N/A"), { r => r.accept = "application/x-msgpack"; r }) shouldBe User(
+      2,
+      "yui",
+      "N/A"
+    )
 
-        // application/x-msgpack test
-        client.get[User]("/user/1", { r => r.accept = "application/x-msgpack"; r }) shouldBe User(1, "leo", "N/A")
-        client.post[User]("/user", User(2, "yui", "N/A"), { r => r.accept = "application/x-msgpack"; r }) shouldBe User(
-          2,
-          "yui",
-          "N/A"
-        )
+    // Get a response as is
+    client.get[Response]("/response").contentString shouldBe "raw response"
 
-        // Get a response as is
-        client.get[Response]("/response").contentString shouldBe "raw response"
+    // Using a custom HTTP header
+    client.get[User]("/user/1", addRequestId) shouldBe User(1, "leo", "10")
+    client.getResource[UserRequest, User]("/user/info", UserRequest(2, "kai"), addRequestId) shouldBe User(
+      2,
+      "kai",
+      "10"
+    )
+    client.getResource[UserRequest, User]("/user/info2", UserRequest(2, "kai"), addRequestId) shouldBe User(
+      2,
+      "kai",
+      "10"
+    )
 
-        // Using a custom HTTP header
-        client.get[User]("/user/1", addRequestId) shouldBe User(1, "leo", "10")
-        client.getResource[UserRequest, User]("/user/info", UserRequest(2, "kai"), addRequestId) shouldBe User(
-          2,
-          "kai",
-          "10"
-        )
-        client.getResource[UserRequest, User]("/user/info2", UserRequest(2, "kai"), addRequestId) shouldBe User(
-          2,
-          "kai",
-          "10"
-        )
+    client.list[Seq[User]]("/user", addRequestId) shouldBe Seq(User(1, "leo", "10"))
 
-        client.list[Seq[User]]("/user", addRequestId) shouldBe Seq(User(1, "leo", "10"))
+    client.post[User]("/user", User(2, "yui", "N/A"), addRequestId) shouldBe User(2, "yui", "10")
+    client.postOps[User, User]("/user", User(2, "yui", "N/A"), addRequestId) shouldBe User(2, "yui", "10")
+    client
+      .postRaw[User](
+        "/user",
+        User(2, "yui", "N/A"),
+        addRequestId
+      ).contentString shouldBe """{"id":2,"name":"yui","requestId":"10"}"""
 
-        client.post[User]("/user", User(2, "yui", "N/A"), addRequestId) shouldBe User(2, "yui", "10")
-        client.postOps[User, User]("/user", User(2, "yui", "N/A"), addRequestId) shouldBe User(2, "yui", "10")
-        client
-          .postRaw[User](
-            "/user",
-            User(2, "yui", "N/A"),
-            addRequestId
-          ).contentString shouldBe """{"id":2,"name":"yui","requestId":"10"}"""
+    client.put[User]("/user", User(10, "aina", "N/A"), addRequestId) shouldBe User(10, "aina", "10")
+    client.putOps[User, User]("/user", User(10, "aina", "N/A"), addRequestId) shouldBe User(10, "aina", "10")
+    client
+      .putRaw[User](
+        "/user",
+        User(10, "aina", "N/A"),
+        addRequestId
+      ).contentString shouldBe """{"id":10,"name":"aina","requestId":"10"}"""
 
-        client.put[User]("/user", User(10, "aina", "N/A"), addRequestId) shouldBe User(10, "aina", "10")
-        client.putOps[User, User]("/user", User(10, "aina", "N/A"), addRequestId) shouldBe User(10, "aina", "10")
-        client
-          .putRaw[User](
-            "/user",
-            User(10, "aina", "N/A"),
-            addRequestId
-          ).contentString shouldBe """{"id":10,"name":"aina","requestId":"10"}"""
+    client.patch[User]("/user", User(20, "joy", "N/A"), addRequestId) shouldBe User(20, "joy", "10")
+    client.patchOps[User, User]("/user", User(20, "joy", "N/A"), addRequestId) shouldBe User(20, "joy", "10")
+    client
+      .patchRaw[User](
+        "/user",
+        User(20, "joy", "N/A"),
+        addRequestId
+      ).contentString shouldBe """{"id":20,"name":"joy","requestId":"10"}"""
 
-        client.patch[User]("/user", User(20, "joy", "N/A"), addRequestId) shouldBe User(20, "joy", "10")
-        client.patchOps[User, User]("/user", User(20, "joy", "N/A"), addRequestId) shouldBe User(20, "joy", "10")
-        client
-          .patchRaw[User](
-            "/user",
-            User(20, "joy", "N/A"),
-            addRequestId
-          ).contentString shouldBe """{"id":20,"name":"joy","requestId":"10"}"""
-
-        client.delete[User]("/user/1", addRequestId) shouldBe User(1, "xxx", "10")
-        client.deleteOps[DeleteRequestBody, User]("/user/1", DeleteRequestBody(true), addRequestId) shouldBe User(
-          1,
-          "xxx",
-          "10"
-        )
-        client.deleteRaw("/user/1", addRequestId).contentString shouldBe """{"id":1,"name":"xxx","requestId":"10"}"""
-      }
-    }
+    client.delete[User]("/user/1", addRequestId) shouldBe User(1, "xxx", "10")
+    client.deleteOps[DeleteRequestBody, User]("/user/1", DeleteRequestBody(true), addRequestId) shouldBe User(
+      1,
+      "xxx",
+      "10"
+    )
+    client.deleteRaw("/user/1", addRequestId).contentString shouldBe """{"id":1,"name":"xxx","requestId":"10"}"""
   }
 
-  def `fail request`: Unit = {
-    d.build[FinagleServer] { server =>
-      withResource(
-        Finagle.client
-          .withMaxRetry(3)
-          .withBackOff(initialIntervalMillis = 1)
-          .newSyncClient(server.localAddress)
-      ) { client =>
-        warn("Starting http client failure tests")
+  def `fail request`(server: FinagleServer): Unit = {
+    withResource(
+      Finagle.client
+        .withMaxRetry(3)
+        .withBackOff(initialIntervalMillis = 1)
+        .newSyncClient(server.localAddress)
+    ) { client =>
+      warn("Starting http client failure tests")
 
-        {
-          // Test max retry failure
-          val ex = intercept[HttpClientMaxRetryException] {
-            val resp = client.get[String]("/busy")
-          }
-          warn(ex.getMessage)
-          ex.retryContext.retryCount shouldBe 3
-          ex.retryContext.maxRetry shouldBe 3
-          val cause = ex.retryContext.lastError.asInstanceOf[HttpClientException]
-          cause.status shouldBe HttpStatus.InternalServerError_500
+      {
+        // Test max retry failure
+        val ex = intercept[HttpClientMaxRetryException] {
+          val resp = client.get[String]("/busy")
         }
+        warn(ex.getMessage)
+        ex.retryContext.retryCount shouldBe 3
+        ex.retryContext.maxRetry shouldBe 3
+        val cause = ex.retryContext.lastError.asInstanceOf[HttpClientException]
+        cause.status shouldBe HttpStatus.InternalServerError_500
+      }
 
-        {
-          // Non retryable response
-          val cause = intercept[HttpClientException] {
-            client.get[String]("/forbidden")
-          }
-          warn(cause.getMessage)
-          cause.status shouldBe HttpStatus.Forbidden_403
+      {
+        // Non retryable response
+        val cause = intercept[HttpClientException] {
+          client.get[String]("/forbidden")
         }
+        warn(cause.getMessage)
+        cause.status shouldBe HttpStatus.Forbidden_403
+      }
+
+      {
+        // Parse invalid json response
+        val e = intercept[Exception] {
+          client.get[User]("/user/1/invalid")
+        }
+        warn(e)
       }
     }
   }
