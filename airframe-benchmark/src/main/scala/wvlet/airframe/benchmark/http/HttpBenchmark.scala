8ddf3200e1378c5c4dbe42d29e3d8a9f4c8c3718
Taro L. Saito <leo@xerial.org>
2020-08-21 13:42:22 -0700

    airframe-grpc: Add benchmark for grpc-java (#1251)
    * airframe-grpc: Add benchmark for ScalaPB
    * Remove ScalaPB benchmark because it doesn't support Scala 2.11

--- airframe-benchmark/src/main/scala/wvlet/airframe/benchmark/http/HttpBenchmark.scala
+++ airframe-benchmark/src/main/scala/wvlet/airframe/benchmark/http/HttpBenchmark.scala
@@ -12,19 +12,23 @@
  * limitations under the License.
  */
 package wvlet.airframe.benchmark.http
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{Executors, TimeUnit}
 
+import com.google.common.util.concurrent.{FutureCallback, Futures}
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.Future
-import io.grpc.Channel
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder
 import io.grpc.stub.StreamObserver
+import io.grpc.{Channel, ManagedChannelBuilder}
 import org.openjdk.jmh.annotations._
 import org.openjdk.jmh.infra.Blackhole
 import wvlet.airframe.Session
+import wvlet.airframe.benchmark.http.protojava.ProtoJavaGreeter
 import wvlet.airframe.http.finagle.{Finagle, FinagleClient, FinagleServer, FinagleSyncClient}
 import wvlet.airframe.http.grpc.gRPC
 import wvlet.log.LogSupport
+import wvlet.log.io.IOUtil
 
 object HttpBenchmark {
   final val asyncIteration = 100
@@ -37,7 +41,7 @@ import wvlet.airframe.benchmark.http.HttpBenchmark._
 @State(Scope.Benchmark)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
-class FinagleBenchmark extends LogSupport {
+class AirframeFinagle extends LogSupport {
 
   private val design =
     Finagle.server
@@ -79,7 +83,7 @@ class FinagleBenchmark extends LogSupport {
       }
     }
     while (counter.get() != asyncIteration) {
-      Thread.`yield`()
+      Thread.sleep(0)
     }
   }
 }
@@ -87,7 +91,7 @@ class FinagleBenchmark extends LogSupport {
 @State(Scope.Benchmark)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
-class GrpcBenchmark extends LogSupport {
+class AirframeGrpc extends LogSupport {
 
   private val design =
     gRPC.server.withRouter(Greeter.router).designWithChannel.withProductionMode
@@ -134,8 +138,136 @@ class GrpcBenchmark extends LogSupport {
       )
     }
     while (counter.get() != asyncIteration) {
-      Thread.`yield`()
+      Thread.sleep(0)
+    }
+  }
+}
+
+//@State(Scope.Benchmark)
+//@BenchmarkMode(Array(Mode.Throughput))
+//@OutputTimeUnit(TimeUnit.SECONDS)
+//class ScalaPB extends LogSupport {
+//
+//  private val port                        = IOUtil.randomPort
+//  private var client: GreeterBlockingStub = null
+//  private var asyncClient: GreeterStub    = null
+//  private val executionContext            = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
+//  private val server =
+//    NettyServerBuilder.forPort(port).addService(GreeterGrpc.bindService(new ScalaPBGreeter(), executionContext)).build()
+//  private val channel = ManagedChannelBuilder.forTarget(s"localhost:${port}").usePlaintext().build()
+//
+//  @Setup
+//  def setup: Unit = {
+//    server.start
+//    client = new GreeterBlockingStub(channel)
+//    asyncClient = new GreeterStub(channel)
+//  }
+//
+//  @TearDown
+//  def teardown: Unit = {
+//    server.shutdown
+//    channel.shutdownNow
+//    executionContext.shutdownNow()
+//  }
+//
+//  @Benchmark
+//  def rpcSync(blackhole: Blackhole): Unit = {
+//    blackhole.consume(client.hello(HelloRequest("RPC")))
+//  }
+//
+//  @Benchmark
+//  @OperationsPerInvocation(asyncIteration)
+//  def rpcAsync(blackhole: Blackhole): Unit = {
+//    val counter = new AtomicInteger(0)
+//    val futures = for (i <- 0 until asyncIteration) yield {
+//      val f = asyncClient
+//        .hello(HelloRequest("RPC"))
+//      f.onComplete(x => counter.incrementAndGet())(executionContext)
+//      f
+//    }
+//    while (counter.get() != asyncIteration) {
+//      Thread.sleep(0)
+//    }
+//  }
+//}
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+class GrpcJava extends LogSupport {
+
+  private val port                                                       = IOUtil.randomPort
+  private var client: protojava.GreeterGrpc.GreeterBlockingStub          = null
+  private var asyncClient: protojava.GreeterGrpc.GreeterStub             = null
+  private val executor                                                   = Executors.newCachedThreadPool()
+  private var futureAsyncClient: protojava.GreeterGrpc.GreeterFutureStub = null
+  private val server =
+    NettyServerBuilder.forPort(port).addService(new ProtoJavaGreeter).build()
+  private val channel = ManagedChannelBuilder.forTarget(s"localhost:${port}").usePlaintext().build()
+
+  @Setup
+  def setup: Unit = {
+    server.start
+    client = protojava.GreeterGrpc.newBlockingStub(channel)
+    asyncClient = protojava.GreeterGrpc.newStub(channel)
+    futureAsyncClient = protojava.GreeterGrpc.newFutureStub(channel)
+  }
+
+  @TearDown
+  def teardown: Unit = {
+    server.shutdown
+    channel.shutdownNow
+    executor.shutdownNow()
+  }
+
+  @Benchmark
+  def rpcSync(blackhole: Blackhole): Unit = {
+    blackhole.consume(client.sayHello(protojava.HelloRequest.newBuilder().setName("RPC").build()))
+  }
+
+  @Benchmark
+  @OperationsPerInvocation(asyncIteration)
+  def rpcFutureAsync(blackhole: Blackhole): Unit = {
+    val counter = new AtomicInteger(0)
+    for (i <- 0 until asyncIteration) yield {
+      val future = futureAsyncClient.sayHello(protojava.HelloRequest.newBuilder().setName("RPC").build())
+      Futures.addCallback(
+        future,
+        new FutureCallback[protojava.HelloReply] {
+          override def onSuccess(result: protojava.HelloReply): Unit = {
+            blackhole.consume(result.getMessage)
+            counter.incrementAndGet()
+          }
+          override def onFailure(t: Throwable): Unit = ???
+        },
+        executor
+      )
+    }
+    while (counter.get() != asyncIteration) {
+      Thread.sleep(0)
     }
   }
 
+  @Benchmark
+  @OperationsPerInvocation(asyncIteration)
+  def rpcAsync(blackhole: Blackhole): Unit = {
+    val counter = new AtomicInteger(0)
+    for (i <- 0 until asyncIteration) yield {
+      asyncClient.sayHello(
+        protojava.HelloRequest.newBuilder().setName("RPC").build(),
+        new StreamObserver[protojava.HelloReply] {
+          override def onNext(v: protojava.HelloReply): Unit = {
+            blackhole.consume(v.getMessage)
+          }
+          override def onError(t: Throwable): Unit = {}
+          override def onCompleted(): Unit = {
+            counter.incrementAndGet()
+          }
+        }
+      )
+    }
+    while (counter.get() != asyncIteration) {
+      Thread.sleep(0)
+    }
+  }
 }
--- build.sbt
+++ build.sbt
@@ -698,8 +698,7 @@ lazy val benchmark =
   project
     .in(file("airframe-benchmark"))
     // Necessary for generating /META-INF/BenchmarkList
-    .enablePlugins(JmhPlugin)
-    .enablePlugins(PackPlugin)
+    .enablePlugins(JmhPlugin, PackPlugin)
     .settings(buildSettings)
     .settings(
       name := "airframe-benchmark",
@@ -722,8 +721,15 @@ lazy val benchmark =
         "org.openjdk.jmh" % "jmh-generator-reflection" % JMH_VERSION,
         // Used only for json benchmark
         "org.json4s" %% "json4s-jackson" % "3.6.9",
-        "io.circe"   %% "circe-parser"   % "0.11.2"
+        "io.circe"   %% "circe-parser"   % "0.11.2",
+        // For ScalaPB
+        // "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % scalapb.compiler.Version.scalapbVersion
+        // For grpc-java
+        "io.grpc" % "grpc-protobuf" % GRPC_VERSION
       ),
+//      PB.targets in Compile := Seq(
+//        scalapb.gen() -> (sourceManaged in Compile).value / "scalapb"
+//      ),
       publishPackArchiveTgz
     )
     .dependsOn(msgpackJVM, jsonJVM, metricsJVM, launcher, finagle, grpc, airframeMacrosJVMRef, airspecRefJVM % Test)
