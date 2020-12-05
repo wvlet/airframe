fd6fe687015c5e7170b24bbe9dd60bc42f932baa
Taro L. Saito <leo@xerial.org>
2020-12-05 10:46:23 -0800

    Relocate code for Scala 3

--- airframe-surface/shared/src/main/scala/wvlet/airframe/surface/Surfaces.scala
+++ airframe-surface/shared/src/main/scala/wvlet/airframe/surface/Surfaces.scala
@@ -15,6 +15,86 @@ package wvlet.airframe.surface
 
 import scala.language.existentials
 
+sealed trait ParameterBase extends Serializable {
+  def name: String
+  def surface: Surface
+
+  def call(obj: Any, x: Any*): Any
+}
+
+trait Parameter extends ParameterBase {
+  def index: Int
+  def name: String
+
+  /**
+    * Surface for representing this parameter type
+    */
+  def surface: Surface
+
+  /**
+    * Returns true if this parameter has @required annotation
+    */
+  def isRequired: Boolean
+
+  /**
+    * Returns true if this parameter has @required annotation
+    */
+  def isSecret: Boolean
+
+  /**
+    * Get this parameter value from a given object x
+    */
+  def get(x: Any): Any
+
+  override def call(obj: Any, x: Any*): Any = get(obj)
+
+  /**
+    * Get the default value of this parameter.
+    * For example the default value of x in class A(x:Int = 10) is 10
+    *
+    * @return
+    */
+  def getDefaultValue: Option[Any]
+}
+
+/**
+  */
+trait ObjectFactory extends Serializable {
+  def newInstance(args: Seq[Any]): Any
+}
+
+case class MethodRef(owner: Class[_],
+                     name: String,
+                     paramTypes: Seq[Class[_]],
+                     isConstructor: Boolean)
+
+trait MethodParameter extends Parameter {
+  def method: MethodRef
+
+  /**
+    * Method owner instance is necessary to find by-name parameter default values
+    * @param methodOwner
+    * @return
+    */
+  def getMethodArgDefaultValue(methodOwner: Any): Option[Any] = getDefaultValue
+}
+
+trait MethodSurface extends ParameterBase {
+  def mod: Int
+  def owner: Surface
+  def name: String
+  def args: Seq[MethodParameter]
+  def surface: Surface = returnType
+  def returnType: Surface
+
+  def isPublic: Boolean = (mod & MethodModifier.PUBLIC) != 0
+  def isPrivate: Boolean = (mod & MethodModifier.PRIVATE) != 0
+  def isProtected: Boolean = (mod & MethodModifier.PROTECTED) != 0
+  def isStatic: Boolean = (mod & MethodModifier.STATIC) != 0
+  def isFinal: Boolean = (mod & MethodModifier.FINAL) != 0
+  def isAbstract: Boolean = (mod & MethodModifier.ABSTRACT) != 0
+}
+
 /**
   * Parameters of a Surface
   */
@@ -28,8 +108,8 @@ case class StdMethodParameter(
     private val defaultValue: Option[Any] = None,
     accessor: Option[Any => Any] = None
 ) extends MethodParameter {
-  override def toString: String             = s"${name}:${surface.name}"
-  def get(x: Any): Any                      = accessor.map(a => a(x)).getOrElse(null)
+  override def toString: String = s"${name}:${surface.name}"
+  def get(x: Any): Any = accessor.map(a => a(x)).getOrElse(null)
   override def getDefaultValue: Option[Any] = defaultValue
 }
 
@@ -38,80 +118,86 @@ object Primitive {
 
   private[surface] val primitiveTable = {
     val b = Map.newBuilder[Class[_], PrimitiveSurface]
-    b += classOf[jl.Boolean]   -> Boolean
-    b += classOf[Boolean]      -> Boolean
-    b += classOf[jl.Short]     -> Short
-    b += classOf[Short]        -> Short
-    b += classOf[jl.Byte]      -> Byte
-    b += classOf[Byte]         -> Byte
+    b += classOf[jl.Boolean] -> Boolean
+    b += classOf[Boolean] -> Boolean
+    b += classOf[jl.Short] -> Short
+    b += classOf[Short] -> Short
+    b += classOf[jl.Byte] -> Byte
+    b += classOf[Byte] -> Byte
     b += classOf[jl.Character] -> Char
-    b += classOf[Char]         -> Char
-    b += classOf[jl.Integer]   -> Int
-    b += classOf[Int]          -> Int
-    b += classOf[jl.Float]     -> Float
-    b += classOf[Float]        -> Float
-    b += classOf[jl.Long]      -> Long
-    b += classOf[Long]         -> Long
-    b += classOf[jl.Double]    -> Double
-    b += classOf[Double]       -> Double
-    b += classOf[jl.String]    -> String
-    b += classOf[String]       -> String
+    b += classOf[Char] -> Char
+    b += classOf[jl.Integer] -> Int
+    b += classOf[Int] -> Int
+    b += classOf[jl.Float] -> Float
+    b += classOf[Float] -> Float
+    b += classOf[jl.Long] -> Long
+    b += classOf[Long] -> Long
+    b += classOf[jl.Double] -> Double
+    b += classOf[Double] -> Double
+    b += classOf[jl.String] -> String
+    b += classOf[String] -> String
     b.result
   }
 
   def apply(cl: Class[_]): PrimitiveSurface = primitiveTable(cl)
 
-  sealed abstract class PrimitiveSurface(rawType: Class[_]) extends GenericSurface(rawType) {
+  sealed abstract class PrimitiveSurface(rawType: Class[_])
+      extends GenericSurface(rawType) {
     override def isPrimitive: Boolean = true
   }
 
   case object Int extends PrimitiveSurface(classOf[Int]) {
-    override def name: String     = "Int"
+    override def name: String = "Int"
     override def fullName: String = "Int"
   }
   case object Byte extends PrimitiveSurface(classOf[Byte]) {
-    override def name: String     = "Byte"
+    override def name: String = "Byte"
     override def fullName: String = "Byte"
   }
   case object Long extends PrimitiveSurface(classOf[Long]) {
-    override def name: String     = "Long"
+    override def name: String = "Long"
     override def fullName: String = "Long"
   }
   case object Short extends PrimitiveSurface(classOf[Short]) {
-    override def name: String     = "Short"
+    override def name: String = "Short"
     override def fullName: String = "Short"
   }
   case object Boolean extends PrimitiveSurface(classOf[Boolean]) {
-    override def name: String     = "Boolean"
+    override def name: String = "Boolean"
     override def fullName: String = "Boolean"
   }
   case object Float extends PrimitiveSurface(classOf[Float]) {
-    override def name: String     = "Float"
+    override def name: String = "Float"
     override def fullName: String = "Float"
   }
   case object Double extends PrimitiveSurface(classOf[Double]) {
-    override def name: String     = "Double"
+    override def name: String = "Double"
     override def fullName: String = "Double"
   }
   case object Char extends PrimitiveSurface(classOf[Char]) {
-    override def name: String     = "Char"
+    override def name: String = "Char"
     override def fullName: String = "Char"
   }
   case object String extends PrimitiveSurface(classOf[String])
 
   case object Unit extends PrimitiveSurface(classOf[Unit]) {
-    override def name: String     = "Unit"
+    override def name: String = "Unit"
     override def fullName: String = "Unit"
   }
 }
 
-case class Alias(override val name: String, override val fullName: String, ref: Surface)
-    extends GenericSurface(ref.rawType, ref.typeArgs, ref.params, ref.objectFactory) {
-  override def toString: String     = s"${name}:=${ref.name}"
-  override def isAlias: Boolean     = true
+case class Alias(override val name: String,
+                 override val fullName: String,
+                 ref: Surface)
+    extends GenericSurface(ref.rawType,
+                           ref.typeArgs,
+                           ref.params,
+                           ref.objectFactory) {
+  override def toString: String = s"${name}:=${ref.name}"
+  override def isAlias: Boolean = true
   override def isPrimitive: Boolean = ref.isPrimitive
-  override def isOption: Boolean    = ref.isOption
-  override def dealias: Surface     = ref.dealias
+  override def isOption: Boolean = ref.isOption
+  override def dealias: Surface = ref.dealias
 }
 
 case class HigherKindedTypeSurface(
@@ -127,47 +213,51 @@ case class HigherKindedTypeSurface(
       s"${name}[${typeArgs.mkString(",")}]"
     }
   }
-  override def isAlias: Boolean     = false
+  override def isAlias: Boolean = false
   override def isPrimitive: Boolean = ref.isPrimitive
-  override def isOption: Boolean    = ref.isOption
-  override def dealias: Surface     = ref.dealias
+  override def isOption: Boolean = ref.isOption
+  override def dealias: Surface = ref.dealias
 }
 
 case object ExistentialType extends GenericSurface(classOf[Any]) {
-  override def name: String     = "_"
+  override def name: String = "_"
   override def fullName: String = "_"
 }
 
 case class ArraySurface(override val rawType: Class[_], elementSurface: Surface)
     extends GenericSurface(rawType, Seq(elementSurface)) {
-  override def name: String     = s"Array[${elementSurface.name}]"
+  override def name: String = s"Array[${elementSurface.name}]"
   override def fullName: String = s"Array[${elementSurface.fullName}]"
   override def toString: String = name
 }
 
-case class OptionSurface(override val rawType: Class[_], elementSurface: Surface)
+case class OptionSurface(override val rawType: Class[_],
+                         elementSurface: Surface)
     extends GenericSurface(rawType, Seq(elementSurface)) {
   override def isOption: Boolean = true
 }
-case class JavaEnumSurface(override val rawType: Class[_]) extends GenericSurface(rawType)
-
-case class EnumSurface(override val rawType: Class[_], stringExtractor: (Class[_], String) => Option[Any])
+case class JavaEnumSurface(override val rawType: Class[_])
     extends GenericSurface(rawType)
 
-case class TupleSurface(override val rawType: Class[_], override val typeArgs: Seq[Surface])
+case class EnumSurface(override val rawType: Class[_],
+                       stringExtractor: (Class[_], String) => Option[Any])
+    extends GenericSurface(rawType)
+
+case class TupleSurface(override val rawType: Class[_],
+                        override val typeArgs: Seq[Surface])
     extends GenericSurface(rawType, typeArgs) {}
 
 case class TaggedSurface(base: Surface, tag: Surface) extends Surface {
-  override def toString: String       = name
-  override def rawType: Class[_]      = base.rawType
+  override def toString: String = name
+  override def rawType: Class[_] = base.rawType
   override def typeArgs: Seq[Surface] = base.typeArgs
   override def params: Seq[Parameter] = base.params
-  override def name: String           = s"${base.name}@@${tag.name}"
-  override def fullName: String       = s"${base.fullName}@@${tag.fullName}"
-  override def isOption: Boolean      = base.isOption
-  override def isAlias: Boolean       = base.isAlias
-  override def isPrimitive: Boolean   = base.isPrimitive
-  override def dealias: Surface       = base.dealias
+  override def name: String = s"${base.name}@@${tag.name}"
+  override def fullName: String = s"${base.fullName}@@${tag.fullName}"
+  override def isOption: Boolean = base.isOption
+  override def isAlias: Boolean = base.isAlias
+  override def isPrimitive: Boolean = base.isPrimitive
+  override def dealias: Surface = base.dealias
 
   override def objectFactory: Option[ObjectFactory] = base.objectFactory
 }
@@ -216,8 +306,8 @@ class GenericSurface(
     }
   }
 
-  def isOption: Boolean    = false
-  def isAlias: Boolean     = false
+  def isOption: Boolean = false
+  def isAlias: Boolean = false
   def isPrimitive: Boolean = false
 
   override def toString: String = name
@@ -249,12 +339,12 @@ case class LazySurface(rawType: Class[_], fullName: String) extends Surface {
     }
   }
 
-  override def toString: String                     = name
-  override def params                               = ref.params
-  override def typeArgs: Seq[Surface]               = ref.typeArgs
-  override def isOption                             = ref.isOption
-  override def isAlias                              = ref.isAlias
-  override def isPrimitive                          = ref.isPrimitive
+  override def toString: String = name
+  override def params = ref.params
+  override def typeArgs: Seq[Surface] = ref.typeArgs
+  override def isOption = ref.isOption
+  override def isAlias = ref.isAlias
+  override def isPrimitive = ref.isPrimitive
   override def objectFactory: Option[ObjectFactory] = ref.objectFactory
 }
 
@@ -267,10 +357,14 @@ case class ClassMethodSurface(
     methodCaller: Option[(Any, Seq[Any]) => Any]
 ) extends MethodSurface {
   override def call(obj: Any, x: Any*) = {
-    def unsupported = throw new UnsupportedOperationException(s"Calling method ${name} is not supported: ${this}")
+    def unsupported =
+      throw new UnsupportedOperationException(
+        s"Calling method ${name} is not supported: ${this}")
 
     methodCaller
-      .map { caller => caller(obj, x.toSeq) }
+      .map { caller =>
+        caller(obj, x.toSeq)
+      }
       .getOrElse {
         unsupported
       }
