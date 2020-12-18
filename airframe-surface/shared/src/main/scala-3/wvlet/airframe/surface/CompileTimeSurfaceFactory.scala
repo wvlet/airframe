f7cafc3cc18ef3fca87ff591e92a7448d4cdfbf5
Taro L. Saito <leo@xerial.org>
2020-12-17 16:55:20 -0800

    Add generic factories

--- airframe-surface/shared/src/main/scala-3/wvlet/airframe/surface/CompileTimeSurfaceFactory.scala
+++ airframe-surface/shared/src/main/scala-3/wvlet/airframe/surface/CompileTimeSurfaceFactory.scala
@@ -45,7 +45,12 @@ class CompileTimeSurfaceFactory(using quotes:Quotes) {
   import quotes._
   import quotes.reflect._
 
+
   private def fullTypeNameOf(t:Type[_]): String = {
+     fullTypeNameOf(TypeRepr.of(using t))
+  }
+
+  private def fullTypeNameOf(t:TypeRepr): String = {
       def sanitize(symbol:Symbol): String = {
         val fullName = symbol.fullName
         fullName.split("\\.").toList match {
@@ -59,14 +64,14 @@ class CompileTimeSurfaceFactory(using quotes:Quotes) {
             fullName.replaceAll("\\$", "")
         }
       }
-      val tt = TypeTree.of(using t)
-      tt.tpe match {
+      t match {
         case a:AppliedType if a.args.nonEmpty =>
           s"${sanitize(a.typeSymbol)}[${a.args.map(pt => fullTypeNameOf(pt.asType)).mkString(",")}]"
         case other => 
           sanitize(other.typeSymbol)
       }
   }
+
   private type Factory = PartialFunction[TypeRepr, Expr[Surface]]
 
   def surfaceOf(tpe: Type[_]): Expr[Surface] = {
@@ -75,7 +80,16 @@ class CompileTimeSurfaceFactory(using quotes:Quotes) {
 
   private def surfaceOf(tr: TypeRepr): Expr[Surface] = {
     val nullFactory: Expr[Surface] = '{null}
-    primitiveTypeFactory.applyOrElse(tr, { x => nullFactory } )
+    
+    println(s"${typeNameOf(tr)}: ${tr}")
+    factory.applyOrElse(tr, { x => nullFactory } )
+  }
+
+  private def factory: Factory = {
+    taggedTypeFactory orElse
+    aliasFactory orElse
+    primitiveTypeFactory orElse
+    genericTypeFactory
   }
 
   private def primitiveTypeFactory: Factory = {
@@ -91,5 +105,57 @@ class CompileTimeSurfaceFactory(using quotes:Quotes) {
     case t if t =:= TypeRepr.of[Unit] => '{ Primitive.Unit }
   }
 
-  
+  private def typeNameOf(t: TypeRepr): String = {
+    t.typeSymbol.fullName
+  }
+
+  private def isTaggedType(t: TypeRepr): Boolean = {
+    typeNameOf(t).startsWith("wvlet.airframe.surface.tag.")
+  }
+
+  private def taggedTypeFactory: Factory = {
+    case a : AppliedType if a.args.length == 2 && isTaggedType(a) =>
+      '{ TaggedSurface(${surfaceOf(a.args(0))}, ${surfaceOf(a.args(1))}) }
+  }
+
+  private def belongsToScalaDefault(t: TypeRepr): Boolean = {
+    val scalaDefaultPackages = Seq("scala.", "scala.Predef$.", "scala.util.")
+    val nme = t.typeSymbol.fullName
+    scalaDefaultPackages.exists(p => nme.startsWith(p))
+  }
+
+  private def aliasFactory: Factory = {
+    case t if t.typeSymbol.isType && t.typeSymbol.isAliasType && !belongsToScalaDefault(t) =>
+      val dealiased = t.dealias
+      val inner = if(t != dealiased) {
+        surfaceOf(dealiased)
+      }
+      else {
+        surfaceOf(t.simplified)
+      }
+      val s = t.typeSymbol
+      val name = Expr(s.name)
+      val fullName = Expr(fullTypeNameOf(t.asType))
+      '{ Alias(${name}, ${fullName}, ${inner}) } 
+  }
+
+
+  private def clsOf(t:TypeRepr): Expr[Class[_]] = {
+    Literal(ClassOfConstant(t)).asExpr.asInstanceOf[Expr[Class[_]]]
+  }
+
+  private def newGenericSurfaceOf(t:TypeRepr): Expr[Surface] = {
+    '{ new GenericSurface(${clsOf(t)}) }
+  }
+
+  private def genericTypeFactory: Factory = {
+    case a: AppliedType =>
+      val typeArgs = a.args.map(surfaceOf(_))
+      '{ new GenericSurface(${clsOf(a)}, typeArgs = ${Expr.ofSeq(typeArgs)}.toIndexedSeq) }
+    case r: Refinement =>
+      newGenericSurfaceOf(r.info)
+    case t =>
+      newGenericSurfaceOf(t)
+  }
+
 }
--- airframe-surface/shared/src/test/scala-3/wvlet/airframe/surface/Surface3Test.scala
+++ airframe-surface/shared/src/test/scala-3/wvlet/airframe/surface/Surface3Test.scala
@@ -2,6 +2,7 @@ package test
 
 import wvlet.airframe.surface.Surface
 import wvlet.log.LogSupport
+import wvlet.airframe.surface.tag._
 
 object Surface3Test extends LogSupport {
   import scala.quoted._
@@ -21,6 +22,8 @@ object Surface3Test extends LogSupport {
 
   case class Person(id:Int, name:String)
 
+  trait Label 
+  type MyString = String
 
   def main(args: Array[String]): Unit = {
     test(Surface.of[Int], "Int")
@@ -29,6 +32,8 @@ object Surface3Test extends LogSupport {
     test(Surface.of[Seq[Int]], "Seq[Int]")
     test(Surface.of[Map[String, Int]], "Map[String,Int]")
     test(Surface.of[Person], "Person")
+    test(Surface.of[String @@ Label], "String@@Label")
+    test(Surface.of[MyString], "MyString:=String")
   }
   // ddddd
 
