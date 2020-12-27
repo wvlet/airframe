02e497edebaacff0eabd6586529fb1e1a7ea4a32
Taro L. Saito <leo@xerial.org>
2020-12-26 17:27:05 -0800

    Implement shouldGenerateTraitOf

--- airframe/src/main/scala-3/wvlet/airframe/DesignImpl.scala
+++ airframe/src/main/scala-3/wvlet/airframe/DesignImpl.scala
@@ -44,16 +44,48 @@ private[airframe] object DesignMacros {
   def shouldGenerateTraitOf[A](using quotes:Quotes, tpe: Type[A]): Boolean = {
     import quotes.reflect._
     val t = TypeRepr.of(using tpe)
+    val ts = t.typeSymbol
 
+    // Has a publid default constructor that can be called without any argument?
     val hasPublicDefaultConstructor = {
+      val pc = ts.primaryConstructor
+      pc.paramSymss.size == 1 && pc.paramSymss(0).size == 0
+    }
+    val hasAbstractMethods = {
+      ts.memberMethods.exists(m => m.flags.is(Flags.Abstract))
+    }
+    val isTaggedType = ts.fullName.startsWith("wvlet.airframe.surface.tag.")
+    val isSealedType = ts.flags.is(Flags.Sealed)
+    val shouldInstantiateTrait = if(!ts.flags.is(Flags.Static)) {
+      // = Non static type
+      // If X is non static type (= local class or trait),
+      // we need to instantiate it first in order to populate its $outer variables
 
-        //t.primaryConstructor.filter(_.isNoSym)
+      // We cannot instantiate path-dependent types
+      if(ts.fullName.contains("#")) {
+        false
+      } 
+      else {
+        hasPublicDefaultConstructor
+      }
+    } else if (ts.flags.is(Flags.Abstract)) {
+      // = Abstract type
+      // We cannot build abstract type X that has abstract methods, so bind[X].to[ConcreteType]
+      // needs to be found in the design
+
+      // If there is no abstract methods, it might be a trait without any method
+      !hasAbstractMethods
+    } else {
+      // We cannot instantiate any trait or class without the default constructor
+      // So binding needs to be found in the Design.
+      hasPublicDefaultConstructor
     }
 
-    true
+    // Tagged type or sealed class binding should be found in Design
+    !isTaggedType && !isSealedType && shouldInstantiateTrait
   }
 
-  def registerTraitFactory[A](using quotes: Quotes, tpe: Type[A]): Expr[Surface] = {
+  def registerTraitFactoryOf[A](using quotes: Quotes, tpe: Type[A]): Expr[Surface] = {
     if(shouldGenerateTraitOf[A]) {
       '{
         val __surface = Surface.of[A]
@@ -71,7 +103,7 @@ private[airframe] object DesignMacros {
 
   def designBind[A](design:Expr[Design])(using quotes:Quotes, tpe: Type[A]): Expr[Binder[A]] = {
     '{
-        val __surface = ${registerTraitFactory[A]}
+        val __surface = ${registerTraitFactoryOf[A]}
         new Binder(${design}, __surface, SourceCode()).asInstanceOf[Binder[A]]
      }
   }
