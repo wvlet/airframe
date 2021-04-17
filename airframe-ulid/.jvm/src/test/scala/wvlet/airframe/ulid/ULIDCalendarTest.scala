d29d904d6c19211cb36ba4618b5e24d54d4ee4ea
Taro L. Saito <leo@xerial.org>
2021-04-17 09:08:45 -0700

    Move test

--- airframe-ulid/.jvm/src/test/scala/wvlet/airframe/ulid/ULIDCalendarTest.scala
+++ airframe-ulid/.jvm/src/test/scala/wvlet/airframe/ulid/ULIDCalendarTest.scala
@@ -11,7 +11,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package wvlet.airframe.control
+package wvlet.airframe.ulid
+
 import java.util.Calendar
 import wvlet.airspec.AirSpec
 import wvlet.airspec.spi.PropertyCheck
