16b53ca7d68b62cd805e71bb3eb7514774f41417
Taro L. Saito <leo@xerial.org>
2021-05-14 21:30:55 -0700

    Add provider bindings

--- airframe-di/src/main/scala-3/wvlet/airframe/di/BinderImpl.scala
+++ airframe-di/src/main/scala-3/wvlet/airframe/di/BinderImpl.scala
@@ -18,15 +18,15 @@ private[di] trait BinderImpl[A] extends LogSupport { self: Binder[A] =>
 
   inline def toProvider[D1](factory: D1 => A): DesignWithContext[A] = ${ BinderMacros.toProvider1[D1, A]('self, 'factory, true, false) }
   inline def toProvider[D1, D2](factory: (D1, D2) => A): DesignWithContext[A] = ${ BinderMacros.toProvider2[D1, D2, A]('self, 'factory, true, false) }
-  def toProvider[D1, D2, D3](factory: (D1, D2, D3) => A): DesignWithContext[A] = ???
-  def toProvider[D1, D2, D3, D4](factory: (D1, D2, D3, D4) => A): DesignWithContext[A] = ???
-  def toProvider[D1, D2, D3, D4, D5](factory: (D1, D2, D3, D4, D5) => A): DesignWithContext[A] = ???
+  inline def toProvider[D1, D2, D3](factory: (D1, D2, D3) => A): DesignWithContext[A] = ${ BinderMacros.toProvider3[D1, D2, D3, A]('self, 'factory, true, false) }
+  inline def toProvider[D1, D2, D3, D4](factory: (D1, D2, D3, D4) => A): DesignWithContext[A] = ${ BinderMacros.toProvider4[D1, D2, D3, D4, A]('self, 'factory, true, false) }
+  inline def toProvider[D1, D2, D3, D4, D5](factory: (D1, D2, D3, D4, D5) => A): DesignWithContext[A] = ${ BinderMacros.toProvider5[D1, D2, D3, D4, D5, A]('self, 'factory, true, false) }
 
   inline def toEagerSingletonProvider[D1](factory: D1 => A): DesignWithContext[A] = ${ BinderMacros.toProvider1[D1, A]('self, 'factory, true, true) }
   inline def toEagerSingletonProvider[D1, D2](factory: (D1, D2) => A): DesignWithContext[A] = ${ BinderMacros.toProvider2[D1, D2, A]('self, 'factory, true, true) }
-  def toEagerSingletonProvider[D1, D2, D3](factory: (D1, D2, D3) => A): DesignWithContext[A] = ???
-  def toEagerSingletonProvider[D1, D2, D3, D4](factory: (D1, D2, D3, D4) => A): DesignWithContext[A] = ???
-  def toEagerSingletonProvider[D1, D2, D3, D4, D5](factory: (D1, D2, D3, D4, D5) => A): DesignWithContext[A] = ???
+  inline def toEagerSingletonProvider[D1, D2, D3](factory: (D1, D2, D3) => A): DesignWithContext[A] = ${ BinderMacros.toProvider3[D1, D2, D3, A]('self, 'factory, true, true) }
+  inline def toEagerSingletonProvider[D1, D2, D3, D4](factory: (D1, D2, D3, D4) => A): DesignWithContext[A] = ${ BinderMacros.toProvider4[D1, D2, D3, D4, A]('self, 'factory, true, true) }
+  inline def toEagerSingletonProvider[D1, D2, D3, D4, D5](factory: (D1, D2, D3, D4, D5) => A): DesignWithContext[A] = ${ BinderMacros.toProvider5[D1, D2, D3, D4, D5, A]('self, 'factory, true, true) }
 }
 
 private[di] object BinderMacros {
@@ -71,6 +71,63 @@ private[di] object BinderMacros {
         val d2 = Surface.of[D2]
         self.design.addBinding[A](ProviderBinding(
           DependencyFactory(self.from, Seq(d1, d2), ${factory}),
+          ${Expr.apply(singleton)},
+          ${Expr.apply(eager)},
+          self.sourceCode)
+        )
+      }
+    }
+  }
+
+  def toProvider3[D1, D2, D3, A](binder: Expr[Binder[_]], factory: Expr[(D1, D2, D3) => A], singleton:Boolean, eager:Boolean)
+          (using Type[D1], Type[D2], Type[D3], Type[A], Quotes): Expr[DesignWithContext[A]] = {
+    '{
+      {
+        val self = ${binder}
+        val d1 = Surface.of[D1]
+        val d2 = Surface.of[D2]
+        val d3 = Surface.of[D3]
+        self.design.addBinding[A](ProviderBinding(
+          DependencyFactory(self.from, Seq(d1, d2, d3), ${factory}),
+          ${Expr.apply(singleton)},
+          ${Expr.apply(eager)},
+          self.sourceCode)
+        )
+      }
+    }
+  }
+
+  def toProvider4[D1, D2, D3, D4, A](binder: Expr[Binder[_]], factory: Expr[(D1, D2, D3, D4) => A], singleton:Boolean, eager:Boolean)
+          (using Type[D1], Type[D2], Type[D3], Type[D4], Type[A], Quotes): Expr[DesignWithContext[A]] = {
+    '{
+      {
+        val self = ${binder}
+        val d1 = Surface.of[D1]
+        val d2 = Surface.of[D2]
+        val d3 = Surface.of[D3]
+        val d4 = Surface.of[D4]
+        self.design.addBinding[A](ProviderBinding(
+          DependencyFactory(self.from, Seq(d1, d2, d3, d4), ${factory}),
+          ${Expr.apply(singleton)},
+          ${Expr.apply(eager)},
+          self.sourceCode)
+        )
+      }
+    }
+  }
+
+  def toProvider5[D1, D2, D3, D4, D5, A](binder: Expr[Binder[_]], factory: Expr[(D1, D2, D3, D4, D5) => A], singleton:Boolean, eager:Boolean)
+          (using Type[D1], Type[D2], Type[D3], Type[D4], Type[D5], Type[A], Quotes): Expr[DesignWithContext[A]] = {
+    '{
+      {
+        val self = ${binder}
+        val d1 = Surface.of[D1]
+        val d2 = Surface.of[D2]
+        val d3 = Surface.of[D3]
+        val d4 = Surface.of[D4]
+        val d5 = Surface.of[D5]
+        self.design.addBinding[A](ProviderBinding(
+          DependencyFactory(self.from, Seq(d1, d2, d3, d4, d5), ${factory}),
           ${Expr.apply(singleton)},
           ${Expr.apply(eager)},
           self.sourceCode)
