b169480ceb5e1da5872f90a2dcacb2cf3f3cac37
Taro L. Saito <leo@xerial.org>
2021-05-22 20:35:40 -0700

    Bridge runtime class and compile-time Surface generator

--- airframe-surface/.jvm/src/test/scala-3/wvlet/airframe/surface/reflect/TastySurfaceFactoryTest.scala
+++ airframe-surface/.jvm/src/test/scala-3/wvlet/airframe/surface/reflect/TastySurfaceFactoryTest.scala
@@ -2,13 +2,21 @@ package wvlet.airframe.surface.reflect
 
 import wvlet.airspec.AirSpec
 
-class Person(id:Int, name:String)
+case class Person(id:Int, name:String)
 
 object TastySurfaceFactoryTest extends AirSpec {
 
   test("..") {
 
-    TastySurfaceFactory.ofClass(classOf[Person])
+    val s = TastySurfaceFactory.ofClass(classOf[Int])
+    info(s)
 
+    info(ReflectSurfaceFactory.ofClass(classOf[Person]))
+
+  }
+
+  test("of[A]") {
+    val s = TastySurfaceFactory.of[Person]
+    info(s.params.mkString(", "))
   }
 }
