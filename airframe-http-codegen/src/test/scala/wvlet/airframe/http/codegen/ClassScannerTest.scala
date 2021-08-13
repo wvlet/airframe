34f14445bc78f4db5d1a6c5157699e0bd104bab6
Taro L. Saito <leo@xerial.org>
2021-08-13 16:43:09 -0700

    airframe-surface: Fixes #1607 Give up building Surface for difficult types

--- airframe-http-codegen/src/test/scala/wvlet/airframe/http/codegen/ClassScannerTest.scala
+++ airframe-http-codegen/src/test/scala/wvlet/airframe/http/codegen/ClassScannerTest.scala
@@ -13,6 +13,7 @@
  */
 package wvlet.airframe.http.codegen
 
+import wvlet.airframe.surface.reflect.ReflectSurfaceFactory
 import wvlet.airspec.AirSpec
 
 /**
@@ -22,5 +23,11 @@ class ClassScannerTest extends AirSpec {
     ClassScanner.decodePath(
       "/lib/0.0.1%2Btest/xxxx-0.0.1%2Btest.jar"
     ) shouldBe "/lib/0.0.1+test/xxxx-0.0.1+test.jar"
+  }
+
+  test("Skip abstract class") {
+    val cl = classOf[io.grpc.stub.AbstractStub[_]]
+    val s  = ReflectSurfaceFactory.ofClass(cl)
+    //val m  = ReflectSurfaceFactory.methodsOfClass(cl)
   }
 }
--- airframe-surface/.jvm/src/main/scala-2/wvlet/airframe/surface/reflect/ReflectSurfaceFactory.scala
+++ airframe-surface/.jvm/src/main/scala-2/wvlet/airframe/surface/reflect/ReflectSurfaceFactory.scala
@@ -99,7 +99,7 @@ object ReflectSurfaceFactory extends LogSupport {
   def findTypeOf(s: Surface): Option[ru.Type] = typeMap.get(s)
 
   def get(name: String): Surface = {
-    surfaceCache.getOrElse(name, throw new IllegalArgumentException(s"Surface ${name} is not found in cache"))
+    surfaceCache.getOrElse(name, throw new NoSuchElementException(s"Surface ${name} is not found in cache"))
   }
 
   private def typeNameOf(t: ru.Type): String = {
@@ -323,11 +323,18 @@ object ReflectSurfaceFactory extends LogSupport {
             trace(f"Resolving the unknown type $tpe into AnyRef")
             new GenericSurface(resolveClass(tpe))
           }
-          val surface = m(tpe)
+          val surface =
+            try {
+              m(tpe)
+            } catch {
+              case e: NoSuchElementException =>
+                // Failed to create surface (Not found in cache)
+                AnyRefSurface
+            }
           // Cache if not yet cached
           surfaceCache.getOrElseUpdate(fullName, surface)
           typeMap.getOrElseUpdate(surface, tpe)
-          trace(s"surfaceOf(${tpe}) Surface: ${surface}, Surace class:${surface.getClass}, tpe: ${showRaw(tpe)}")
+          trace(s"surfaceOf(${tpe}) Surface: ${surface}, Surface class:${surface.getClass}, tpe: ${showRaw(tpe)}")
           surface
         }
       } catch {
@@ -391,7 +398,6 @@ object ReflectSurfaceFactory extends LogSupport {
         wvlet.airframe.surface.ExistentialType
       case t @ TypeRef(NoPrefix, tpe, args) if !t.typeSymbol.isClass =>
         val name = tpe.name.decodedName.toString
-
         HigherKindedTypeSurface(name, name, surfaceOf(t.erasure), args.map(ta => surfaceOf(ta)))
     }
 
