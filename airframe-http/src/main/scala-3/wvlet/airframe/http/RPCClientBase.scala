5b2ea5f0fdcb4065f767a2b93ff33cff1d51ab78
Taro L. Saito <leo@xerial.org>
2022-05-09 17:03:03 -0700

    Scala 3 support

---
+++ airframe-http/src/main/scala-3/wvlet/airframe/http/RPCClientBase.scala
@@ -0,0 +1,29 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wvlet.airframe.http
+
+import wvlet.airframe.surface.Surface
+import wvlet.airframe.http.HttpMessage.{Response,Request}
+/**
+  * Scala 3 specific helper method to make an RPC request
+  */
+trait RPCSyncClientBase { self: RPCSyncClient =>
+  inline def send[RequestType, ResponseType](
+      resourcePath: String,
+      request: RequestType,
+      requestFilter: Request => Request
+  ): ResponseType = {
+    self.sendRaw(resourcePath, Surface.of[RequestType], request, Surface.of[ResponseType], requestFilter).asInstanceOf[ResponseType]
+  }
+}
