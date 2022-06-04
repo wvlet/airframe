e33ce183e9f596be296c2a682d841e4366a79f3a
Taro L. Saito <leo@xerial.org>
2022-06-04 11:03:01 -0700

    Use only sed for dynver computation (#2233)
    Use only sed for dynver calc

--- scripts/dynver.sh
+++ scripts/dynver.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # Compute sbt-dynver-compatible version number
 BUILD_TIME=`date '+%Y%m%d-%H%M'`
 DYN_VER=`git describe --long --tags --abbrev=8 --match "v[0-9]*" --always --dirty="-${BUILD_TIME}"`
-GIT_DIST=`echo ${DYN_VER} | ruby -pe "gsub(/v([^-]*)-([0-9]+)-g(.*)/, '\2')"`
+GIT_DIST=`echo ${DYN_VER} | sed -re "s/v([^-]*)-([0-9]+)-g(.*)/\2/g"`
 GIT_TAG=`git describe --tags --dirty`
-RELEASE_VERSION=`echo ${DYN_VER} | ruby -pe "gsub(/v([^-]*)-([0-9]+)-g(.*)/, '\1')"`
-SNAPSHOT_VERSION=`echo ${DYN_VER} | ruby -pe "gsub(/v([^-]*)-([0-9]+)-g(.*)/, '\1-\2-\3')"`-SNAPSHOT
+RELEASE_VERSION=`echo ${DYN_VER} | sed -re "s/v([^-]*)-([0-9]+)-g(.*)/\1/g"`
+SNAPSHOT_VERSION=`echo ${DYN_VER} | sed -re "s/v([^-]*)-([0-9]+)-g(.*)/\1-\2-\3/g"`-SNAPSHOT
 
 if [ ${GIT_DIST} -eq 0 ]; then
   if [ ${GIT_TAG} == *"-dirty" ]; then
