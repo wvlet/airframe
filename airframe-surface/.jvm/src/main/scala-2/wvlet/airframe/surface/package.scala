c66f98018a0f358aaad399bc407c91993272b5cf
Taro L. Saito <leo@xerial.org>
2023-04-25 15:29:12 -0700

    Add surfaceCache for Scala 2.xx + Scala.js

--- airframe-surface/.jvm/src/main/scala-2/wvlet/airframe/surface/package.scala
+++ airframe-surface/.jvm/src/main/scala-2/wvlet/airframe/surface/package.scala
@@ -15,11 +15,15 @@ package wvlet.airframe
 
 import wvlet.airframe.surface.reflect.ReflectSurfaceFactory
 
+import java.util.concurrent.ConcurrentHashMap
 import scala.collection.mutable
+import scala.jdk.CollectionConverters._
 
 /**
   */
 package object surface {
+  val surfaceCache = new ConcurrentHashMap[String, Surface]().asScala
+
   def getCached(fullName: String): Surface = ReflectSurfaceFactory.get(fullName)
   def newCacheMap[A, B]: mutable.Map[A, B] = new mutable.WeakHashMap[A, B]()
 }
