e2da16b5cf388faee0d446e0b2cd4a0a2ac31f62
Taro L. Saito <leo@xerial.org>
2023-04-27 15:40:38 -0700

    Fix for js

--- airframe-http/src/main/scala-3/wvlet/airframe/http/RxRouterBase.scala
+++ airframe-http/src/main/scala-3/wvlet/airframe/http/RxRouterBase.scala
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package wvlet.airframe.http.router
+package wvlet.airframe.http
 
 import wvlet.airframe.http.RxHttpFilter
 import wvlet.airframe.http.RxRouter
