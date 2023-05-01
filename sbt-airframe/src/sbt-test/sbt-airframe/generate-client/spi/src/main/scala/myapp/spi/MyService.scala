185d96484c4166e3db8437ae0a612c06feafeee2
Taro L. Saito <leo@xerial.org>
2023-05-01 16:34:34 -0700

    fix example

--- sbt-airframe/src/sbt-test/sbt-airframe/generate-client/spi/src/main/scala/myapp/spi/MyService.scala
+++ sbt-airframe/src/sbt-test/sbt-airframe/generate-client/spi/src/main/scala/myapp/spi/MyService.scala
@@ -6,7 +6,7 @@ trait MyService {
   @Endpoint(method = HttpMethod.GET, path = "/v1/hello/:id")
   def hello(id: Int): String
 
-  @Endpoint(method = HttpMethod.GET, path = "/v1/books")
+  @Endpoint(method = HttpMethod.POST, path = "/v1/books")
   def books(limit: Int = 100): String
 }
 
