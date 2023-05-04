5ae7b76d3968f71f2361fd037bcd3ee609dcf875
Taro L. Saito <leo@xerial.org>
2023-05-03 23:16:15 -0700

    Improve error message

--- airframe-http/src/main/scala/wvlet/airframe/http/HttpClient.scala
+++ airframe-http/src/main/scala/wvlet/airframe/http/HttpClient.scala
@@ -173,7 +173,12 @@ object HttpClient extends LogSupport {
         val adapter = implicitly[HttpRequestAdapter[Req]]
         val req     = r.asInstanceOf[Req]
         val path    = adapter.pathOf(req)
-        s"Request to ${path} is failed: ${ctx.lastError.getMessage}"
+        ctx.lastError.getCause match {
+          case re: RPCException =>
+            s"Request to ${path} is failed: ${re.getMessage}"
+          case _ =>
+            s"Request to ${path} is failed: ${ctx.lastError.getMessage}"
+        }
       case _ =>
         s"Request is failed: ${ctx.lastError.getMessage}"
     }
