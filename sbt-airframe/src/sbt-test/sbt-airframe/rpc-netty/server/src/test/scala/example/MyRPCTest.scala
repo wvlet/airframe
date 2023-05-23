3dd82f6c8e7f21973ba9341293b72769a38c7911
Taro L. Saito <leo@xerial.org>
2023-05-23 15:09:53 -0700

    internal: Recover disabled sbt-airframe scripted test (#2980)

--- sbt-airframe/src/sbt-test/sbt-airframe/rpc-netty/server/src/test/scala/example/MyRPCTest.scala
+++ sbt-airframe/src/sbt-test/sbt-airframe/rpc-netty/server/src/test/scala/example/MyRPCTest.scala
@@ -10,20 +10,16 @@ import example.api.MyRPCClient
 import example.api.MyRPCClient.RPCSyncClient
 
 class MyRPCTest extends AirSpec {
-
-  test("RPC test") {
-    pending("airspec 23.5.5 is required to bind RPCSyncClient properly")
+  override protected def design: Design = {
+    Netty.server.withRouter(MyRPCApi.router).design
+      .bind[RPCSyncClient].toProvider[NettyServer] { (server: NettyServer) =>
+      MyRPCClient.newRPCSyncClient(Http.client.newSyncClient(server.localAddress))
+    }
   }
-//  override protected def design: Design = {
-//    Netty.server.withRouter(MyRPCApi.router).design
-//      .bind[RPCSyncClient].toProvider[NettyServer] { (server: NettyServer) =>
-//      MyRPCClient.newRPCSyncClient(Http.client.newSyncClient(server.localAddress))
-//    }
-//  }
-//
-//  test("Access RPC") { (client: RPCSyncClient) =>
-//    val ret = client.MyRPCApi.helloRPC(HelloRequest("Airframe"))
-//    info(ret)
-//    ret shouldBe HelloResponse("Hello Airframe!")
-//  }
+
+  test("Access RPC") { (client: RPCSyncClient) =>
+    val ret = client.MyRPCApi.helloRPC(HelloRequest("Airframe"))
+    info(ret)
+    ret shouldBe HelloResponse("Hello Airframe!")
+  }
 }
