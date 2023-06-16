503e526d863cee971e579ffedba240cb1ff44b4e
Taro L. Saito <leo@xerial.org>
2023-06-16 09:24:42 -0700

    airspec: Fixes #3014 Do not evaluate tests returning Nil or Seq[_] before the execution (#3019)

--- airspec/src/main/scala-3/wvlet/airspec/AirSpecSpiCompat.scala
+++ airspec/src/main/scala-3/wvlet/airspec/AirSpecSpiCompat.scala
@@ -21,17 +21,17 @@ private[airspec] trait AirSpecSpiCompat { self: AirSpecSpi =>
 
 class AirSpecTestBuilder(val spec: AirSpecSpi, val name: String, val design: Design => Design)
     extends wvlet.log.LogSupport {
-  inline def apply[R](body: => R): Unit                  = ${ AirSpecMacros.test0Impl[R]('this, 'body) }
-  inline def apply[D1, R](body: D1 => R): Unit           = ${ AirSpecMacros.test1Impl[R, D1]('this, 'body) }
-  inline def apply[D1, D2, R](body: (D1, D2) => R): Unit = ${ AirSpecMacros.test2Impl[R, D1, D2]('this, 'body) }
-  inline def apply[D1, D2, D3, R](body: (D1, D2, D3) => R): Unit = ${
-    AirSpecMacros.test3Impl[R, D1, D2, D3]('this, 'body)
+  inline def apply[R](inline body: => R): Unit                  = ${ AirSpecMacros.test0Impl[R]('this, 'body) }
+  inline def apply[D1, R](inline body: D1 => R): Unit           = ${ AirSpecMacros.test1Impl[D1, R]('this, 'body) }
+  inline def apply[D1, D2, R](inline body: (D1, D2) => R): Unit = ${ AirSpecMacros.test2Impl[D1, D2, R]('this, 'body) }
+  inline def apply[D1, D2, D3, R](inline body: (D1, D2, D3) => R): Unit = ${
+    AirSpecMacros.test3Impl[D1, D2, D3, R]('this, 'body)
   }
-  inline def apply[D1, D2, D3, D4, R](body: (D1, D2, D3, D4) => R): Unit = ${
-    AirSpecMacros.test4Impl[R, D1, D2, D3, D4]('this, 'body)
+  inline def apply[D1, D2, D3, D4, R](inline body: (D1, D2, D3, D4) => R): Unit = ${
+    AirSpecMacros.test4Impl[D1, D2, D3, D4, R]('this, 'body)
   }
-  inline def apply[D1, D2, D3, D4, D5, R](body: (D1, D2, D3, D4, D5) => R): Unit = ${
-    AirSpecMacros.test5Impl[R, D1, D2, D3, D4, D5]('this, 'body)
+  inline def apply[D1, D2, D3, D4, D5, R](inline body: (D1, D2, D3, D4, D5) => R): Unit = ${
+    AirSpecMacros.test5Impl[D1, D2, D3, D4, D5, R]('this, 'body)
   }
 }
 
@@ -42,14 +42,7 @@ object AirSpecTestBuilder extends wvlet.log.LogSupport {
       v.spec.addLocalTestDef(AirSpecDefF0(v.name, v.design, r, body))
     }
     def addF1[D1, R](d1: Surface, r: Surface, body: D1 => R): Unit = {
-      val spec = body match {
-        case s: Seq[_] =>
-          // Workaround for: https://github.com/wvlet/airframe/issues/1845
-          AirSpecDefF0(v.name, v.design, r, wvlet.airframe.LazyF0(() => body))
-        case _ =>
-          AirSpecDefF1(v.name, v.design, d1, r, body)
-      }
-      v.spec.addLocalTestDef(spec)
+      v.spec.addLocalTestDef(AirSpecDefF1(v.name, v.design, d1, r, body))
     }
     def addF2[D1, D2, R](d1: Surface, d2: Surface, r: Surface, body: (D1, D2) => R): Unit = {
       v.spec.addLocalTestDef(AirSpecDefF2(v.name, v.design, d1, d2, r, body))
@@ -84,25 +77,36 @@ object AirSpecTestBuilder extends wvlet.log.LogSupport {
 private[airspec] object AirSpecMacros {
   import scala.quoted._
 
-  def test0Impl[R](self: Expr[AirSpecTestBuilder], body: Expr[_])(using Type[R], Quotes): Expr[Unit] = {
+  def test0Impl[R](self: Expr[AirSpecTestBuilder], body: Expr[R])(using Type[R], Quotes): Expr[Unit] = {
     '{
       import AirSpecTestBuilder._
       ${ self }.addF0(Surface.of[R], LazyF0(${ body }))
     }
   }
 
-  def test1Impl[R, D1](self: Expr[AirSpecTestBuilder], body: Expr[D1 => R])(using
+  def test1Impl[D1, R](self: Expr[AirSpecTestBuilder], body: Expr[D1 => R])(using
       Type[R],
       Type[D1],
       Quotes
   ): Expr[Unit] = {
-    '{
-      import AirSpecTestBuilder._
-      ${ self }.addF1(Surface.of[D1], Surface.of[R], ${ body })
+    import quotes.reflect.*
+    body match {
+      // Workaround for https://github.com/wvlet/airframe/issues/1845. If the return type of a test code block is
+      // Nil or Seq[_], Scala 3 complier passes Function1[Int, R] as a body even if the code block take no argument.
+      case '{ $x: t } if TypeRepr.of[t] =:= TypeRepr.of[Nil.type] || TypeRepr.of[t] <:< TypeRepr.of[Seq[_]] =>
+        '{
+          import AirSpecTestBuilder._
+          ${ self }.addF0(Surface.of[R], LazyF0.apply(${ body }))
+        }
+      case _ =>
+        '{
+          import AirSpecTestBuilder._
+          ${ self }.addF1(Surface.of[D1], Surface.of[R], ${ body })
+        }
     }
   }
 
-  def test2Impl[R, D1, D2](
+  def test2Impl[D1, D2, R](
       self: Expr[AirSpecTestBuilder],
       body: Expr[(D1, D2) => R]
   )(using Type[R], Type[D1], Type[D2], Quotes): Expr[Unit] = {
@@ -112,7 +116,7 @@ private[airspec] object AirSpecMacros {
     }
   }
 
-  def test3Impl[R, D1, D2, D3](
+  def test3Impl[D1, D2, D3, R](
       self: Expr[AirSpecTestBuilder],
       body: Expr[(D1, D2, D3) => R]
   )(using Type[R], Type[D1], Type[D2], Type[D3], Quotes): Expr[Unit] = {
@@ -122,7 +126,7 @@ private[airspec] object AirSpecMacros {
     }
   }
 
-  def test4Impl[R, D1, D2, D3, D4](
+  def test4Impl[D1, D2, D3, D4, R](
       self: Expr[AirSpecTestBuilder],
       body: Expr[(D1, D2, D3, D4) => R]
   )(using Type[R], Type[D1], Type[D2], Type[D3], Type[D4], Quotes): Expr[Unit] = {
@@ -132,7 +136,7 @@ private[airspec] object AirSpecMacros {
     }
   }
 
-  def test5Impl[R, D1, D2, D3, D4, D5](
+  def test5Impl[D1, D2, D3, D4, D5, R](
       self: Expr[AirSpecTestBuilder],
       body: Expr[(D1, D2, D3, D4, D5) => R]
   )(using Type[R], Type[D1], Type[D2], Type[D3], Type[D4], Type[D5], Quotes): Expr[Unit] = {
