650f2281ec27fcc7fbad800a8cd6967584c93496
Taro L. Saito <leo@xerial.org>
2023-08-03 22:17:20 -0700

    cleanup

--- airframe-http/src/main/scala/wvlet/airframe/http/client/HttpClients.scala
+++ airframe-http/src/main/scala/wvlet/airframe/http/client/HttpClients.scala
@@ -194,7 +194,7 @@ trait AsyncClient extends AsyncClientCompat with HttpClientFactory[AsyncClient] 
         loggingFilter
           .andThen(config.clientFilter)
           .apply(context)
-          .andThen(req => channel.sendstatAsync(req, config))
+          .andThen(req => channel.sendAsync(req, config))
           .apply(request)
           .map { resp =>
             // Remember the last response for error reporting purpose
