21e10e085c031dc7a2f2adbe0e4ab98ef584afa5
Taro L. Saito <leo@xerial.org>
2023-08-06 12:15:46 -0700

    codec (internal): Use inline methods instead of macros

--- airframe-codec/src/main/scala-3/wvlet/airframe/codec/ScalaCompat.scala
+++ airframe-codec/src/main/scala-3/wvlet/airframe/codec/ScalaCompat.scala
@@ -17,39 +17,14 @@ object ScalaCompat {
   }
 
   trait MessageCodecFactoryBase { self: MessageCodecFactory =>
-    inline def of[A]: MessageCodec[A]       = ${ CodecMacros.factoryOf[A]('self) }
-    inline def fromJson[A](json: String): A = ${ CodecMacros.factoryFromJson[A]('json) }
-    inline def toJson[A](obj: A): String    = ${ CodecMacros.factoryToJson[A]('obj) }
-  }
-
-  import scala.quoted._
-
-  private[codec] object CodecMacros {
-    def codecOf[A](using t: Type[A], quotes: Quotes): Expr[MessageCodec[A]] = {
-      import quotes._
-      '{ MessageCodec.ofSurface(Surface.of[A]).asInstanceOf[MessageCodec[A]] }
+    inline def of[A]: MessageCodec[A] = {
+      self.ofSurface(Surface.of[A]).asInstanceOf[MessageCodec[A]]
     }
-    def codecFromJson[A](json: Expr[String])(using t: Type[A], quotes: Quotes): Expr[A] = {
-      import quotes._
-      '{ MessageCodecFactory.defaultFactory.fromJson[A](${ json }) }
+    inline def fromJson[A](json: String): A = {
+      MessageCodec.of[A].fromJson(json)
     }
-    def codecToJson[A](obj: Expr[A])(using t: Type[A], quotes: Quotes): Expr[String] = {
-      import quotes._
-      '{ MessageCodecFactory.defaultFactory.toJson[A](${ obj }) }
-    }
-
-    def factoryOf[A](self: Expr[MessageCodecFactory])(using t: Type[A], quote: Quotes): Expr[MessageCodec[A]] = {
-      import quotes._
-      '{ ${ self }.ofSurface(Surface.of[A]).asInstanceOf[MessageCodec[A]] }
-    }
-    def factoryFromJson[A](json: Expr[String])(using t: Type[A], quotes: Quotes): Expr[A] = {
-      import quotes._
-      '{ MessageCodec.of[A].fromJson(${ json }) }
-    }
-    def factoryToJson[A](obj: Expr[A])(using t: Type[A], quotes: Quotes): Expr[String] = {
-      import quotes._
-      '{ MessageCodec.of[A].toJson(${ obj }) }
+    inline def toJson[A](obj: A): String = {
+      MessageCodec.of[A].toJson(obj)
     }
   }
-
 }
