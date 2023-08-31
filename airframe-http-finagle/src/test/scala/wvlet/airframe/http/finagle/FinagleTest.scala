703363726d90ef684d73a84fdc5093e3fae3090d
Yukihiro Okada (Yuki) <yukihiro.okada@treasure-data.com>
2023-09-01 06:28:06 +0900

    finagle (fix): Properly set http method and path parameters in convertToFinagleRequest (#3179)
    ## Overview
    
    - Currently, `convertToFinagleRequest` doesn't respect `Method`,
    `request path` and etc
    
    ---------
    
    Co-authored-by: Taro L. Saito <leo@xerial.org>

--- airframe-http-finagle/src/test/scala/wvlet/airframe/http/finagle/FinagleTest.scala
+++ airframe-http-finagle/src/test/scala/wvlet/airframe/http/finagle/FinagleTest.scala
@@ -14,10 +14,9 @@
 package wvlet.airframe.http.finagle
 
 import java.nio.charset.StandardCharsets
-
 import com.twitter.finagle.http
 import com.twitter.finagle.http.Status
-import wvlet.airframe.http.{HttpMultiMap, HttpStatus}
+import wvlet.airframe.http.{HttpMessage, HttpMethod, HttpMultiMap, HttpStatus}
 import wvlet.airspec.AirSpec
 
 /**
@@ -63,5 +62,41 @@ class FinagleTest extends AirSpec {
     r.contentType shouldBe Some("application/json;charset=utf-8")
     r.contentBytes shouldBe "hello world".getBytes(StandardCharsets.UTF_8)
     resp.toRaw shouldBeTheSameInstanceAs resp
+  }
+
+  test("convertToFinagleRequest test") {
+    test("basic") {
+      val req = HttpMessage.Request(HttpMethod.GET, "/foo")
+
+      val response = convertToFinagleRequest(req)
+
+      response.method.name shouldBe "GET"
+      response.path shouldBe "/foo"
+    }
+    test("GET with query parameters") {
+      val req = HttpMessage.Request(HttpMethod.GET, "/foo?bar=true&baz=1234")
+
+      val response = convertToFinagleRequest(req)
+
+      response.method.name shouldBe "GET"
+      response.path shouldBe "/foo"
+      response.params.contains("bar") shouldBe true
+      response.params.contains("baz") shouldBe true
+      response.params.getBoolean("bar") shouldBe Some(true)
+      response.params.getInt("baz") shouldBe Some(1234)
+    }
+    test("POST with request body") {
+      val req = HttpMessage
+        .Request(HttpMethod.POST, "/json")
+        .withContentTypeJson
+        .withContent("""{"bar": "123456"}""")
+
+      val response = convertToFinagleRequest(req)
+
+      response.method.name shouldBe "POST"
+      response.path shouldBe "/json"
+      response.headerMap.get("Content-Type") shouldBe Option("application/json;charset=utf-8")
+      response.contentString shouldBe """{"bar": "123456"}"""
+    }
   }
 }
