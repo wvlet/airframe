9912ff436793da34bc0c6372ed877370460fee3d
Taro L. Saito <leo@xerial.org>
2024-04-20 17:29:21 -0700

    Delete unused tests

--- airspec/.native/src/test/scala-3/native-test/NativeJUnit.scala
+++
@@ -1,8 +0,0 @@
-import org.junit.Assert._
-import org.junit.Test
-
-class MyTest:
-  @Test def nativeTest(): Unit = {
-    println("Hello JUnit in Scala Native")
-    assertTrue("this assertion should pass", true)
-  }
